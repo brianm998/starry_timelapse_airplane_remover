d3200945f9a4db259b1b5d7f6000976673a3da01
Brian Martin <brian.beholden@gmail.com>
2022-10-11 15:12:43 -0700

    sort sequence frames by name

--- Sources/ntar/main.swift
+++ Sources/ntar/main.swift
@@ -8,9 +8,24 @@ if CommandLine.arguments.count < 1 {
     let input_image_sequence_dirname = CommandLine.arguments[1]
     print("will process \(input_image_sequence_dirname)")
     print("on path \(path)")
-    let image_files = list_image_files(atPath: "\(path)/\(input_image_sequence_dirname)")
+    var image_files = list_image_files(atPath: "\(path)/\(input_image_sequence_dirname)")
+    image_files.sort { (lhs: String, rhs: String) -> Bool in
+        let lh = remove_suffix(fromString: lhs)
+        let rh = remove_suffix(fromString: rhs)
+        return lh < rh
+    }
+    print("image_files \(image_files)")
+
     let images = try load(imageFiles: image_files)
     print("loaded images \(images)")
+}
+
+// XXX removes suffix and path
+func remove_suffix(fromString string: String) -> String {
+    let imageURL = NSURL(fileURLWithPath: string, isDirectory: false) as URL
+    let full_path = imageURL.deletingPathExtension().absoluteString
+    var components = full_path.components(separatedBy: "/")
+    return components[components.count-1]
 }
 
 func load(imageFiles: [String]) throws -> [NSImage] {
@@ -32,7 +47,6 @@ func list_image_files(atPath path: String) -> [String] {
         let contents = try FileManager.default.contentsOfDirectory(atPath: path)
         contents.forEach { file in
             if file.hasSuffix(".tif") || file.hasSuffix(".tiff") {
-                print("file \(file)")
                 image_files.append("\(path)/\(file)")
             }
         }
