d0def5b4e84bc1605750d1209045d44174d88ec1
Brian Martin <brian.beholden@gmail.com>
2022-10-19 06:59:11 -0700

    make ShouldPaint.swift more clear

--- Sources/ntar/ShouldPaint.swift
+++ Sources/ntar/ShouldPaint.swift
@@ -1,13 +1,13 @@
 import Foundation
 
-// generated on Wed Oct 19 06:22:54 PDT 2022
+// generated on Wed Oct 19 06:54:51 PDT 2022
 
 // data used from 56 airplane records and 706762 non airplane records
 
 // this is auto generated code, run regenerate_shouldPaint.sh to remake it, DO NOT EDIT BY HAND
 
 // the unix time this file was created
-let paint_group_logic_time = 1666185774
+let paint_group_logic_time = 1666187691
 
 func shouldPaintGroup(min_x: Int, min_y: Int,
                       max_x: Int, max_y: Int,
@@ -32,14 +32,29 @@ func shouldPaintGroup(min_x: Int, min_y: Int,
         aspect_ratio = Double(bounding_box_width)/Double(bounding_box_height)
     }
 
+    // outlier groups smaller aren't airplanes
     if(group_size < 67) { return false } // not airplane
+
+    // outlier groups larger are airplanes
     if(group_size > 521) { return true } // is airplane
+
+    // outlier groups with a smaller aspect ratio are airplanes
     if(aspect_ratio < 0.210526315789474) { return true } // is airplane
+
+    // outlier groups with a higher fill amount aren't airplanes
     if(amount_filled > 0.587962962962963) { return false } // notAirplane
+
+    // outlier groups with a lower fill amount aren't airplanes
     if(amount_filled < 0.188618925831202) { return false } // notAirplane
+
+    // outlier groups with a larger aspect ratio aren't airplanes
     if(aspect_ratio > 0.214285714285714) { return false } // notAirplane
+
+    //  groups with a smaller aspect ratio aren't airplanes
     if(aspect_ratio < 0.184782608695652) { return true } // airplane
+
+    // with 0.214285714285714 > 0.184782608695652,
+    // no outlier groups can reach this point
+
     return false // not airplane
 }
-
-
--- categorize.pl
+++ categorize.pl
@@ -7,6 +7,9 @@ use strict;
 # 56,17,227,1
 # 19,24,143,0
 
+# which is
+# width,height,size,is_airplane
+
 my $airplane_records = [];
 my $non_airplane_records = [];
 
@@ -14,32 +17,36 @@ my $mode = shift;
 
 my $code_only = 0;
 
+# when run without this arg show debug info that helps improve the alg with more data
+# when run with this arg only output the generated swift code
 $code_only = 1 if($mode eq '--generate-swift');
 
-while(<>) {
-    if(/^(\d+),(\d+),(\d+),(\d+)$/) {
-	my ($width, $height, $size, $is_airplane) = ($1, $2, $3, $4);
-	if($is_airplane == 1) {
-	    push @$airplane_records, [$width, $height, $size];
-	} else {
-	    push @$non_airplane_records, [$width, $height, $size];
-	}
+# read all csv records from STDIN
+while (<>) {
+  if (/^(\d+),(\d+),(\d+),(\d+)$/) {
+    my ($width, $height, $size, $is_airplane) = ($1, $2, $3, $4);
+    if ($is_airplane == 1) {
+      push @$airplane_records, [$width, $height, $size];
+    } else {
+      push @$non_airplane_records, [$width, $height, $size];
     }
-}
-
-unless($code_only) {
-  print "we have ",scalar(@$airplane_records)," airplane records\n";
-  print "we have ",scalar(@$non_airplane_records)," non_airplane records\n";
+  }
 }
 
 my $number_airplane_records = scalar(@$airplane_records);
 my $number_non_airplane_records = scalar(@$non_airplane_records);
+
+unless ($code_only) {
+  print "we have $number_airplane_records airplane records\n";
+  print "we have $number_non_airplane_records non_airplane records\n";
+}
 
 my $date = `date`;
 chomp $date;
 
 my $epoc_seconds = time;
 
+# print out the beginning of the swift code
 print <<EOF
 import Foundation
 
@@ -76,150 +83,164 @@ func shouldPaintGroup(min_x: Int, min_y: Int,
     }
 
 EOF
-;
+  ;
 
 my ($airplane_min_size, $airplane_max_size) = size_range($airplane_records);
 my ($non_airplane_min_size, $non_airplane_max_size) = size_range($non_airplane_records);
 
 
-unless($code_only) {
+unless ($code_only) {
   print("    airplanes were [$airplane_min_size, $airplane_max_size] in size out of ",scalar(@$airplane_records)," records\n");
   print("non airplanes were [$non_airplane_min_size, $non_airplane_max_size] in size of ",scalar(@$non_airplane_records)," records\n");
 }
 
-print "    if(group_size < $airplane_min_size) { return false } // not airplane\n";
-print "    if(group_size > $non_airplane_max_size) { return true } // is airplane\n";
-
-if($airplane_min_size < $non_airplane_max_size) {
-  unless($code_only) {
-    print "there is a no gap between airplanes and non airplanes\n";
-    print "there are both airplanes and not airplanes between sizes $airplane_min_size and $non_airplane_max_size\n";
-  }
-
-  my $new_airplane_arr = [];
-
-  foreach my $record (@$airplane_records) {
-    push @$new_airplane_arr, $record if($record->[2] < $non_airplane_max_size);
-  }
-  $airplane_records = $new_airplane_arr;
-
-  my $new_non_airplane_arr = [];
-
-    foreach my $record (@$non_airplane_records) {
-      push @$new_non_airplane_arr, $record if($record->[2] > $airplane_min_size);
-    }
-    $non_airplane_records = $new_non_airplane_arr;
-
-  unless($code_only) {
-    print "we now have ",scalar(@$airplane_records)," airplane records\n";
-    print "we now have ",scalar(@$non_airplane_records)," non_airplane records\n";
-  }
-    my ($airplane_min_aspect, $airplane_max_aspect) = aspect_range($airplane_records);
-    my ($non_airplane_min_aspect, $non_airplane_max_aspect) = aspect_range($non_airplane_records);
-
-  unless($code_only) {
-    print("    airplanes were [$airplane_min_aspect, $airplane_max_aspect] in aspect out of ",scalar(@$airplane_records)," records\n");
-    print("non airplanes were [$non_airplane_min_aspect, $non_airplane_max_aspect] in aspect of ",scalar(@$non_airplane_records)," records\n");
-  }
-
-    # now filter airplanes on aspect ratio
-
-    $new_airplane_arr = [];
-
-    print ("    if(aspect_ratio < $non_airplane_min_aspect) { return true } // is airplane\n");
-
-    foreach my $record (@$airplane_records) {
-      push @$new_airplane_arr, $record if(aspect_ratio_for_record($record) < $non_airplane_min_aspect);
-    }
-    $airplane_records = $new_airplane_arr;
-
-  unless($code_only) {
-    print "we now have ",scalar(@$airplane_records)," airplane records\n";
-  }
-    my ($airplane_min_fill, $airplane_max_fill) = fill_range($airplane_records);
-  my ($non_airplane_min_fill, $non_airplane_max_fill) = fill_range($non_airplane_records);
-    unless($code_only) {
-
-    print("    airplanes were [$airplane_min_fill, $airplane_max_fill] in fill out of ",scalar(@$airplane_records)," records\n");
-      print("non airplanes were [$non_airplane_min_fill, $non_airplane_max_fill] in fill of ",scalar(@$non_airplane_records)," records\n");
-    }
-
-    # now filter on fill percentage
-
-    $new_non_airplane_arr = [];
-
-    print("    if(amount_filled > ",$airplane_max_fill/100,") { return false } // notAirplane\n");
-    print("    if(amount_filled < ",$airplane_min_fill/100,") { return false } // notAirplane\n");
-
-    foreach my $record (@$non_airplane_records) {
-      my $width = $record->[0];
-      my $height = $record->[1];
-      my $size = $record->[2];
-      if($width != 0 && $height != 0) {
-	my $amt_pct = $size/($width*$height)*100;
-
-	if($amt_pct < $airplane_max_fill && $amt_pct > $airplane_min_fill) {
-	  push @$new_non_airplane_arr, $record;
-	}
-      }
-    }
-    $non_airplane_records = $new_non_airplane_arr;
-
-  unless($code_only) {
-    print "we now have ",scalar(@$non_airplane_records)," non_airplane records\n";
-  }
-
-    ($airplane_min_size, $airplane_max_size) = size_range($airplane_records);
-    ($non_airplane_min_size, $non_airplane_max_size) = size_range($non_airplane_records);
-
-  unless($code_only) {
-    print("    airplanes were [$airplane_min_size, $airplane_max_size] in size out of ",scalar(@$airplane_records)," records\n");
-    print("non airplanes were [$non_airplane_min_size, $non_airplane_max_size] in size of ",scalar(@$non_airplane_records)," records\n");
-  }
-
-    ($airplane_min_fill, $airplane_max_fill) = fill_range($airplane_records);
-    ($non_airplane_min_fill, $non_airplane_max_fill) = fill_range($non_airplane_records);
-  unless($code_only) {
-    print("    airplanes were [$airplane_min_fill, $airplane_max_fill] in fill out of ",scalar(@$airplane_records)," records\n");
-    print("non airplanes were [$non_airplane_min_fill, $non_airplane_max_fill] in fill of ",scalar(@$non_airplane_records)," records\n");
-  }
+print "    // outlier groups smaller aren't airplanes\n";
+print "    if(group_size < $airplane_min_size) { return false } // not airplane\n\n";
+print "    // outlier groups larger are airplanes\n";
+print "    if(group_size > $non_airplane_max_size) { return true } // is airplane\n\n";
 
 
-    ($airplane_min_aspect, $airplane_max_aspect) = aspect_range($airplane_records);
-    ($non_airplane_min_aspect, $non_airplane_max_aspect) = aspect_range($non_airplane_records);
-  unless($code_only) {
-    print("    airplanes were [$airplane_min_aspect, $airplane_max_aspect] in aspect out of ",scalar(@$airplane_records)," records\n");
-    print("non airplanes were [$non_airplane_min_aspect, $non_airplane_max_aspect] in aspect of ",scalar(@$non_airplane_records)," records\n");
-  }
-
-    print("    if(aspect_ratio > $non_airplane_min_aspect) { return false } // notAirplane\n");
-    print("    if(aspect_ratio < $airplane_max_aspect) { return true } // airplane\n");
-
-    # XXX filter here by aspect again
-
-    $new_airplane_arr = [];
-    foreach my $record (@$airplane_records) {
-      push @$new_airplane_arr, $record if(aspect_ratio_for_record($record) > $airplane_max_aspect);
-    }
-    $airplane_records = $new_airplane_arr;
-
-    $new_non_airplane_arr = [];
-    foreach my $record (@$non_airplane_records) {
-      push @$new_non_airplane_arr, $record if(aspect_ratio_for_record($record) < $non_airplane_min_aspect);
-    }
-    $non_airplane_records = $new_non_airplane_arr;
-
-    print <<END
-    return false // not airplane
+unless ($code_only) {
+  print "there is a no gap between airplanes and non airplanes\n";
+  print "there are both airplanes and not airplanes between sizes $airplane_min_size and $non_airplane_max_size\n";
 }
 
+my $new_airplane_arr = [];
 
-END
-      ;
-  unless($code_only) {
-    print "we now have ",scalar(@$airplane_records)," airplane records\n";
-    print "we now have ",scalar(@$non_airplane_records)," non_airplane records\n";
+foreach my $record (@$airplane_records) {
+  push @$new_airplane_arr, $record if($record->[2] < $non_airplane_max_size);
+}
+$airplane_records = $new_airplane_arr;
+
+my $new_non_airplane_arr = [];
+
+foreach my $record (@$non_airplane_records) {
+  push @$new_non_airplane_arr, $record if($record->[2] > $airplane_min_size);
+}
+$non_airplane_records = $new_non_airplane_arr;
+
+unless ($code_only) {
+  print "we now have ",scalar(@$airplane_records)," airplane records\n";
+  print "we now have ",scalar(@$non_airplane_records)," non_airplane records\n";
+}
+my ($airplane_min_aspect, $airplane_max_aspect) = aspect_range($airplane_records);
+my ($non_airplane_min_aspect, $non_airplane_max_aspect) = aspect_range($non_airplane_records);
+
+unless ($code_only) {
+  print("    airplanes were [$airplane_min_aspect, $airplane_max_aspect] in aspect out of ",scalar(@$airplane_records)," records\n");
+  print("non airplanes were [$non_airplane_min_aspect, $non_airplane_max_aspect] in aspect of ",scalar(@$non_airplane_records)," records\n");
+}
+
+# now filter airplanes on aspect ratio
+
+$new_airplane_arr = [];
+
+print ("    // outlier groups with a smaller aspect ratio are airplanes\n"); 
+print ("    if(aspect_ratio < $non_airplane_min_aspect) { return true } // is airplane\n\n");
+
+foreach my $record (@$airplane_records) {
+  push @$new_airplane_arr, $record if(aspect_ratio_for_record($record) < $non_airplane_min_aspect);
+}
+$airplane_records = $new_airplane_arr;
+
+unless ($code_only) {
+  print "we now have ",scalar(@$airplane_records)," airplane records\n";
+}
+
+my ($airplane_min_fill, $airplane_max_fill) = fill_range($airplane_records);
+my ($non_airplane_min_fill, $non_airplane_max_fill) = fill_range($non_airplane_records);
+
+unless ($code_only) {
+  print("    airplanes were [$airplane_min_fill, $airplane_max_fill] in fill out of ",scalar(@$airplane_records)," records\n");
+  print("non airplanes were [$non_airplane_min_fill, $non_airplane_max_fill] in fill of ",scalar(@$non_airplane_records)," records\n");
+}
+
+# now filter on fill percentage
+
+$new_non_airplane_arr = [];
+
+print("    // outlier groups with a higher fill amount aren't airplanes\n");
+print("    if(amount_filled > ",$airplane_max_fill/100,") { return false } // notAirplane\n\n");
+print("    // outlier groups with a lower fill amount aren't airplanes\n");
+print("    if(amount_filled < ",$airplane_min_fill/100,") { return false } // notAirplane\n\n");
+
+foreach my $record (@$non_airplane_records) {
+  my $width = $record->[0];
+  my $height = $record->[1];
+  my $size = $record->[2];
+  if ($width != 0 && $height != 0) {
+    my $amt_pct = $size/($width*$height)*100;
+
+    if ($amt_pct < $airplane_max_fill && $amt_pct > $airplane_min_fill) {
+      push @$new_non_airplane_arr, $record;
+    }
   }
+}
+$non_airplane_records = $new_non_airplane_arr;
+
+unless ($code_only) {
+  print "we now have ",scalar(@$non_airplane_records)," non_airplane records\n";
+}
+
+($airplane_min_size, $airplane_max_size) = size_range($airplane_records);
+($non_airplane_min_size, $non_airplane_max_size) = size_range($non_airplane_records);
+
+unless ($code_only) {
+  print("    airplanes were [$airplane_min_size, $airplane_max_size] in size out of ",scalar(@$airplane_records)," records\n");
+  print("non airplanes were [$non_airplane_min_size, $non_airplane_max_size] in size of ",scalar(@$non_airplane_records)," records\n");
+}
+
+($airplane_min_fill, $airplane_max_fill) = fill_range($airplane_records);
+($non_airplane_min_fill, $non_airplane_max_fill) = fill_range($non_airplane_records);
+unless ($code_only) {
+  print("    airplanes were [$airplane_min_fill, $airplane_max_fill] in fill out of ",scalar(@$airplane_records)," records\n");
+  print("non airplanes were [$non_airplane_min_fill, $non_airplane_max_fill] in fill of ",scalar(@$non_airplane_records)," records\n");
+}
+
+($airplane_min_aspect, $airplane_max_aspect) = aspect_range($airplane_records);
+($non_airplane_min_aspect, $non_airplane_max_aspect) = aspect_range($non_airplane_records);
+unless ($code_only) {
+  print("    airplanes were [$airplane_min_aspect, $airplane_max_aspect] in aspect out of ",scalar(@$airplane_records)," records\n");
+  print("non airplanes were [$non_airplane_min_aspect, $non_airplane_max_aspect] in aspect of ",scalar(@$non_airplane_records)," records\n");
+}
+
+print("    // outlier groups with a larger aspect ratio aren't airplanes\n");
+print("    if(aspect_ratio > $non_airplane_min_aspect) { return false } // notAirplane\n\n");
+print("    //  groups with a smaller aspect ratio aren't airplanes\n");
+print("    if(aspect_ratio < $airplane_max_aspect) { return true } // airplane\n\n");
+
+if ($non_airplane_min_aspect > $airplane_max_aspect) {
+  print"    // with $non_airplane_min_aspect > $airplane_max_aspect,\n";
+  print"    // no outlier groups can reach this point\n";
+} else {
+  print"    // with $non_airplane_min_aspect < $airplane_max_aspect,\n";
+  print"    // some outlier groups can reach this point.\n";
+  print"    // They will be assumed to not be airplanes.\n\n";
+  print"    // further work on the logic in $0 should happen.\n";
+}
+
+$new_airplane_arr = [];
+foreach my $record (@$airplane_records) {
+  push @$new_airplane_arr, $record if(aspect_ratio_for_record($record) > $airplane_max_aspect);
+}
+$airplane_records = $new_airplane_arr;
+
+$new_non_airplane_arr = [];
+foreach my $record (@$non_airplane_records) {
+  push @$new_non_airplane_arr, $record if(aspect_ratio_for_record($record) < $non_airplane_min_aspect);
+}
+$non_airplane_records = $new_non_airplane_arr;
+
+print <<END
+
+    return false // not airplane
+}
+END
+  ;
+unless ($code_only) {
+  print "we now have ",scalar(@$airplane_records)," airplane records\n";
+  print "we now have ",scalar(@$non_airplane_records)," non_airplane records\n";
+}
 
 #    foreach my $record (@$airplane_records) {
 #      print "AR: $record->[0] $record->[1] $record->[2] $record->[3]\n";
@@ -229,12 +250,6 @@ END
 #      print "NA: $record->[0] $record->[1] $record->[2] $record->[3]\n";
 #    }
 
-} else {
-  unless($code_only) {
-    print "GREAT!  There is a size gap between $non_airplane_max_size $airplane_min_size\n";
-    print "all airplanes are above $airplane_min_size and all non-airplanes are below  $non_airplane_max_size\n";
-  }
-}
 
 
 ########
@@ -267,7 +282,6 @@ sub fill_range($) {
 	my $size = $record->[2];
 	if($width != 0 && $height != 0) {
 	    my $amt_pct = $size/($width*$height)*100;
-	    
 	    $min = $amt_pct if($amt_pct < $min);
 	    $max = $amt_pct if($amt_pct > $max);
 	}
