08548f33654b0f1d8ddfccd0b6998bddee3e80f2
Brian Martin <brian.beholden@gmail.com>
2022-10-28 11:55:55 -0700

    get FinalProcessor to the point where it can process any number of neighbors in each direction, and will finish frames at the appropriate times.
    right now the only processing being done is to set all groups to be painted, just to make sure it works.

--- Sources/ntar/FinalProcessor.swift
+++ Sources/ntar/FinalProcessor.swift
@@ -16,10 +16,11 @@ import Cocoa
 actor FinalProcessor {
     var frames: [FrameAirplaneRemover?]
     var current_frame_index = 0
-
+    let frame_count: Int
     
     init(numberOfFrames frame_count: Int) {
         frames = [FrameAirplaneRemover?](repeating: nil, count: frame_count)
+        self.frame_count = frame_count
     }
 
     func add(frame: FrameAirplaneRemover, at index: Int) {
@@ -37,21 +38,75 @@ actor FinalProcessor {
     func frame(at index: Int) -> FrameAirplaneRemover? {
         return frames[index]
     }
+
+    func finishAll() {
+        for frame in frames {
+            if let frame = frame {
+                frame.finish()
+            }
+        }
+    }
+
+    let number_neighbors_needed = 1 // in each direction
     
     nonisolated func run() async {
         var done = false
         while(!done) {
-            //Log.i("current_frame_index \(current_frame_index)")
-            if let current_frame = await self.frame(at: current_frame_index) {
-                current_frame.finish()
-                await self.clearFrame(at: current_frame_index)
+            let index_to_process = await current_frame_index
+
+            var images_to_process: [FrameAirplaneRemover] = []
+            
+            var start_index = index_to_process - number_neighbors_needed
+            var end_index = index_to_process + number_neighbors_needed
+            if start_index < 0 {
+                start_index = 0
+            }
+            if end_index >= frame_count {
+                end_index = frame_count - 1
+            }
+
+            Log.i("processing frame index \(index_to_process)")
+            var bad = false
+            for i in start_index ... end_index {
+                if let next_frame = await self.frame(at: i) {
+                    images_to_process.append(next_frame)
+                } else {
+                    bad = true
+                    // XXX bad
+                }
+            }
+            if !bad {
+                self.handle(frames: images_to_process)
                 await self.incrementCurrentFrameIndex()
+
+                if start_index > 0 {
+                    if let frame_to_finish = await self.frame(at: start_index - 1) {
+                        Log.e("finishing frame")
+                        frame_to_finish.finish()
+                        await self.clearFrame(at: start_index - 1)
+                    }
+                }
+                done = await current_frame_index >= frames.count 
+                if done {
+                    Log.e("finishing all remaining frames")
+                    await self.finishAll()
+                }
             } else {
                 sleep(1)        // XXX hardcoded sleep amount
             }
-            done = await current_frame_index >= frames.count 
         }
     }
+
+    nonisolated func handle(frames: [FrameAirplaneRemover]) {
+        // XXX right now nothing
+        Log.e("handle \(frames.count) frames")
+        for frame in frames {
+            for (group_name, should_paint) in frame.should_paint {
+                frame.should_paint[group_name] = true
+            }
+        }
+    }
+
 }
 
 
