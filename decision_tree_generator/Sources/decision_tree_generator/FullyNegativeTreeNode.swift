4eb2824ab71bda7a17f2090c030d9e3af425b412
Brian Martin <brian.beholden@gmail.com>
2023-04-15 17:07:53 -0700

    more moving and renaming

---
+++ decision_tree_generator/Sources/decision_tree_generator/FullyNegativeTreeNode.swift
@@ -0,0 +1,26 @@
+import Foundation
+import NtarCore
+
+// end leaf node which always returns 100% negative
+@available(macOS 10.15, *) 
+struct FullyNegativeTreeNode: SwiftDecisionTree {
+    let indent: Int
+    var swiftCode: String {
+        var indentation = ""
+        for _ in 0..<indent { indentation += "    " }
+        return "\(indentation)return -1"
+    }
+
+    func classification(of outlierGroup: OutlierGroup) async -> Double {
+        return -1
+    }
+
+    public func classification
+      (
+        of types: [OutlierGroup.TreeDecisionType], // parallel
+        and values: [Double]                        // arrays
+      ) -> Double
+    {
+        return -1
+    }
+}
---
+++ decision_tree_generator/Sources/decision_tree_generator/FullyPositiveTreeNode.swift
@@ -0,0 +1,27 @@
+
+import Foundation
+import NtarCore
+
+// end leaf node which always returns 100% positive
+@available(macOS 10.15, *) 
+struct FullyPositiveTreeNode: SwiftDecisionTree {
+    let indent: Int
+    var swiftCode: String {
+        var indentation = ""
+        for _ in 0..<indent { indentation += "    " }
+        return "\(indentation)return 1"
+    }
+
+    func classification(of outlierGroup: OutlierGroup) async -> Double {
+        return 1
+    }
+
+    public func classification
+      (
+        of types: [OutlierGroup.TreeDecisionType], // parallel
+        and values: [Double]                        // arrays
+      ) -> Double
+    {
+        return 1
+    }
+}
