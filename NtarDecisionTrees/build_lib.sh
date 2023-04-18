4de528b83470f9568d1608e74835223c3aa17c2d
Brian Martin <brian.beholden@gmail.com>
2023-04-17 19:32:04 -0700

    add build_lib.sh, which builds a .a file for both x86 and arm

--- NtarDecisionTrees/build_lib.sh
+++ NtarDecisionTrees/build_lib.sh
@@ -1,2 +1,17 @@
+# re-build the categorizers as static .a files to avoid recompiling them
+
+# clean previous builds
 rm -rf .build
-swift build -Xswiftc -emit-module -Xswiftc -emit-library -Xswiftc -static
+
+# build for x86
+swift build --arch x86_64 -Xswiftc -emit-module -Xswiftc -emit-library -Xswiftc -static
+mv *.a .build/x86_64-apple-macosx
+
+# build for arm64
+swift build --arch arm64  -Xswiftc -emit-module -Xswiftc -emit-library -Xswiftc -static
+mv *.a .build/arm64-apple-macosx
+
+# lipo them together into a universal .a file for both archs
+lipo .build/arm64-apple-macosx/libNtarDecisionTrees.a \
+     .build/x86_64-apple-macosx/libNtarDecisionTrees.a \
+     -create -output libNtarDecisionTrees.a
