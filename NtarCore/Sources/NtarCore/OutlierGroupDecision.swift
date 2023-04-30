3bab012d81b9f747c69b8ddb2e95a6e05ea28ce0
Brian Martin <brian.beholden@gmail.com>
2023-04-29 17:51:10 -0700

    update to new classifier 4e34a241.
    using same total data set as 041b5dc1, but 1/9 test/train split instead of 1/1 as before with 041b5dc1.
    
    Even though 041b5dc1 was trained on much of this test data, it still slightly lost to 4e34a241:
    
    17:37:40.5590 | 🚩 INFO | decision_tree_generator.swift@321: For decision Tree 041b5dc1 out of a total of 145945 outlier groups, 98.60495392099764% success good 143909 vs bad 2036
    17:37:40.5590 | 🚩 INFO | decision_tree_generator.swift@321: For decision Tree 4e34a241 out of a total of 145945 outlier groups, 98.61454657576483% success good 143923 vs bad 2022

--- NtarCore/Sources/NtarCore/OutlierGroupDecision.swift
+++ NtarCore/Sources/NtarCore/OutlierGroupDecision.swift
@@ -20,7 +20,7 @@ public enum DecisionSplitType: String {
 public var decisionTrees: [String: NamedOutlierGroupClassifier] = loadOutlierGroupClassifiers()
 
 // try to load this classifier at runtime
-public let currentClassifierName = "041b5dc1"
+public let currentClassifierName = "4e34a241"
 
 @available(macOS 10.15, *)
 public var currentClassifier: NamedOutlierGroupClassifier? = loadCurrentClassifiers()
