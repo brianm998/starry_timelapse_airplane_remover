f382289e1dc690b70eb17db4bddd956a159f6a14
Brian Martin <brian.beholden@gmail.com>
2023-06-28 11:32:12 -0700

    refactor the InitialView

--- gui/star/InitialView.swift
+++ gui/star/InitialView.swift
@@ -8,6 +8,57 @@ struct InitialView: View {
       UserPreferences.shared.sortedSequenceList.count > 0 ?
       UserPreferences.shared.sortedSequenceList[0] : ""      
 
+    var body: some View {
+        VStack {
+            Spacer()
+              .frame(maxHeight: 20)
+            Text("Welcome to The Star,")
+              .font(.largeTitle)
+            Spacer()
+              .frame(maxHeight: 10)
+            Text("The Starry Timelapse Airplane Remover")
+                .font(.largeTitle)
+            Spacer()
+              .frame(maxHeight: 200)
+            Text("Choose an option to get started")
+            
+            HStack {
+                VStack {
+                    HStack {
+                        Button(action: self.loadConfig) {
+                            Text("Load Config").font(.largeTitle)
+                        }.buttonStyle(ShrinkingButton())
+                          .help("Load a json config file from a previous run of star")
+                        
+                        Button(action: self.loadImageSequence) {
+                            Text("Load Image Sequence").font(.largeTitle)
+                        }.buttonStyle(ShrinkingButton())
+                          .help("Load an image sequence yet to be processed by star")
+                    }
+                    if UserPreferences.shared.recentlyOpenedSequencelist.count > 0 {
+                        HStack {
+                            Button(action: self.loadRecent) {
+                                Text("Open Recent").font(.largeTitle)
+                            }.buttonStyle(ShrinkingButton())
+                              .help("open a recently processed sequence")
+                            
+                            Picker("\u{27F6}", selection: $previously_opened_sheet_showing_item) {
+                                let array = UserPreferences.shared.sortedSequenceList
+                                ForEach(array, id: \.self) { option in
+                                    Text(option)
+                                }
+                            }.frame(maxWidth: 500)
+                              .pickerStyle(.menu)
+                        }
+                    }
+                    Spacer()
+                      .frame(maxHeight: 20)
+                }
+            }
+        }
+          .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+    }
+
     func loadConfig()  {
         Log.d("load config")
 
@@ -39,99 +90,47 @@ struct InitialView: View {
         }
     }
 
-    
-    var body: some View {
-        VStack {
-            Spacer()
-              .frame(maxHeight: 20)
-            Text("Welcome to The Star,")
-              .font(.largeTitle)
-            Spacer()
-              .frame(maxHeight: 10)
-            Text("The Starry Timelapse Airplane Remover")
-                .font(.largeTitle)
-            Spacer()
-              .frame(maxHeight: 200)
-            Text("Choose an option to get started")
-            
-            HStack {
-                VStack {
-                    HStack {
-                        Button(action: self.loadConfig) {
-                            Text("Load Config").font(.largeTitle)
-                        }.buttonStyle(ShrinkingButton())
-                          .help("Load a json config file from a previous run of star")
-                        
-                        let loadImageSequence = {
-                            Log.d("load image sequence")
-                            let openPanel = NSOpenPanel()
-                            //openPanel.allowedFileTypes = ["json"]
-                            openPanel.allowsMultipleSelection = false
-                            openPanel.canChooseDirectories = true
-                            openPanel.canChooseFiles = false
-                            let response = openPanel.runModal()
-                            if response == .OK {
-                                if let returnedUrl = openPanel.url
-                                {
-                                    let path = returnedUrl.path
-                                    Log.d("url path \(path)")
-                                    
-                                    viewModel.sequenceLoaded = true
-                                    viewModel.initial_load_in_progress = true
-                                    viewModel.eraserTask = Task.detached(priority: .userInitiated) {
-                                        do {
-                                            await viewModel.startup(withNewImageSequence: path)
-                                            try await viewModel.eraser?.run()
-                                        } catch {
-                                            Log.e("\(error)")
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        
-                        Button(action: loadImageSequence) {
-                            Text("Load Image Sequence").font(.largeTitle)
-                        }.buttonStyle(ShrinkingButton())
-                          .help("Load an image sequence yet to be processed by star")
+    func loadImageSequence() {
+        Log.d("load image sequence")
+        let openPanel = NSOpenPanel()
+        //openPanel.allowedFileTypes = ["json"]
+        openPanel.allowsMultipleSelection = false
+        openPanel.canChooseDirectories = true
+        openPanel.canChooseFiles = false
+        let response = openPanel.runModal()
+        if response == .OK {
+            if let returnedUrl = openPanel.url
+            {
+                let path = returnedUrl.path
+                Log.d("url path \(path)")
+                
+                viewModel.sequenceLoaded = true
+                viewModel.initial_load_in_progress = true
+                viewModel.eraserTask = Task.detached(priority: .userInitiated) {
+                    do {
+                        await viewModel.startup(withNewImageSequence: path)
+                        try await viewModel.eraser?.run()
+                    } catch {
+                        Log.e("\(error)")
                     }
-                    if UserPreferences.shared.recentlyOpenedSequencelist.count > 0 {
-                        HStack {
-                            let loadRecent = {
-                                Log.d("load image sequence")
-                                
-                                viewModel.sequenceLoaded = true
-                                viewModel.initial_load_in_progress = true
-                                
-                                viewModel.eraserTask = Task.detached(priority: .userInitiated) {
-                                    do {
-                                        await viewModel.startup(withConfig: previously_opened_sheet_showing_item)
-                                        try await viewModel.eraser?.run()
-                                    } catch {
-                                        Log.e("\(error)")
-                                    }
-                                }
-                            }
-                            
-                            Button(action: loadRecent) {
-                                Text("Open Recent").font(.largeTitle)
-                            }.buttonStyle(ShrinkingButton())
-                              .help("open a recently processed sequence")
-                            
-                            Picker("\u{27F6}", selection: $previously_opened_sheet_showing_item) {
-                                let array = UserPreferences.shared.sortedSequenceList
-                                ForEach(array, id: \.self) { option in
-                                    Text(option)
-                                }
-                            }.frame(maxWidth: 500)
-                              .pickerStyle(.menu)
-                        }
-                    }
-                    Spacer()
-                      .frame(maxHeight: 20)
                 }
             }
         }
-          .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+    }
+                        
+    func loadRecent() {
+        Log.d("load image sequence")
+        
+        viewModel.sequenceLoaded = true
+        viewModel.initial_load_in_progress = true
+        
+        viewModel.eraserTask = Task.detached(priority: .userInitiated) {
+            do {
+                await viewModel.startup(withConfig: previously_opened_sheet_showing_item)
+                try await viewModel.eraser?.run()
+            } catch {
+                Log.e("\(error)")
+            }
+        }
     }
 }
