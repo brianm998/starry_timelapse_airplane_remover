9a8b6d13b0071c80c03979d612fe073d0421851a
Brian Martin <brian.beholden@gmail.com>
2023-06-28 18:09:56 -0700

    cleanup

--- StarCore/Sources/StarCore/Types.swift
+++ StarCore/Sources/StarCore/Types.swift
@@ -1,22 +1,23 @@
 import Foundation
 
-extension String: Error {}
-
 // these values are used for creating an initial config when processing a new sequence
 public struct Defaults {
 
     /*
-     The outlier max and min thresholds below are percentages by which a pixel
-     needs to brighten compared to adjecent frames to be considered an outlier.
+     The outlier max and min thresholds below are percentages of how much a
+     pixel is brighter than the same pixel in adjecent frames.
 
      If a pixel is outlierMinThreshold percentage brighter, then it is an outlier.
      If a pixel is brighter than outlierMaxThreshold, then it is painted over fully.
      If a pixel is between these two values, then an alpha between 0-1 is applied.
      */
-    public static let outlierMaxThreshold: Double = 13 // XXX document these
+    public static let outlierMaxThreshold: Double = 13
     public static let outlierMinThreshold: Double = 9
-    public static let minGroupSize: Int = 80      // groups smaller than this are completely ignored
+    public static let minGroupSize: Int = 80 // groups smaller than this are completely ignored
 }
+
+// make any string into an Error, so it can be thrown by itself if desired
+extension String: Error {}
 
 // x, y coordinates
 public struct Coord: Codable {
@@ -41,10 +42,4 @@ public enum Edge {
     case horizontal
 }
 
-// a member in an airplane streak across frames
-typealias AirplaneStreakMember = (
-  frame_index: Int,
-  group: OutlierGroup,
-  distance: Double?      // the distance from this member to the previous one, nil if first member
-)
 
