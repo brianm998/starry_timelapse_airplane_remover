40531a3124890826b5e60e294f97497188643ab8
Brian Martin <brian.beholden@gmail.com>
2023-06-28 18:07:39 -0700

    add comments

--- StarCore/Sources/StarCore/Types.swift
+++ StarCore/Sources/StarCore/Types.swift
@@ -4,6 +4,15 @@ extension String: Error {}
 
 // these values are used for creating an initial config when processing a new sequence
 public struct Defaults {
+
+    /*
+     The outlier max and min thresholds below are percentages by which a pixel
+     needs to brighten compared to adjecent frames to be considered an outlier.
+
+     If a pixel is outlierMinThreshold percentage brighter, then it is an outlier.
+     If a pixel is brighter than outlierMaxThreshold, then it is painted over fully.
+     If a pixel is between these two values, then an alpha between 0-1 is applied.
+     */
     public static let outlierMaxThreshold: Double = 13 // XXX document these
     public static let outlierMinThreshold: Double = 9
     public static let minGroupSize: Int = 80      // groups smaller than this are completely ignored
