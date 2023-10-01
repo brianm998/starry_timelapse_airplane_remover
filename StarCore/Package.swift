f74f08ef000fe2c01ff3b6a890e31614457bf66e
Brian Martin <brian.beholden@gmail.com>
2023-10-01 05:57:29 -0700

    add ShellOut to StarCore Package.swift

--- StarCore/Package.swift
+++ StarCore/Package.swift
@@ -17,6 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -24,6 +25,7 @@ let package = Package(
         .target(
             name: "StarCore",
             dependencies: [
+              .product(name: "ShellOut", package: "ShellOut"),
             ]),
         .testTarget(
             name: "StarCoreTests",
