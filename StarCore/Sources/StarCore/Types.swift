276624fdd7c54640cfea9a400e9d57e05c2050b4
Brian Martin <brian.beholden@gmail.com>
2023-11-11 10:39:07 -0800

    add CartesianLine, and accessor to convert polar line into cartesian

--- StarCore/Sources/StarCore/Types.swift
+++ StarCore/Sources/StarCore/Types.swift
@@ -43,6 +43,78 @@ public struct Line: Codable {
     public let theta: Double                 // angle in degrees
     public let rho: Double                   // distance in pixels
     public let count: Int                    // higher count is better fit for line
+
+    // convert to rise over run, y intercept if possible
+    public var cartesianLine: CartesianLine? {
+        // the line goes through this point
+        let line_intersection_x = self.rho*cos(self.theta*Double.pi/180)
+        let line_intersection_y = self.rho*sin(self.theta*Double.pi/180)
+
+        var edge_1_x = 0.0
+        var edge_1_y = 0.0
+
+        var y_intercept = 0.0
+        if self.theta == 0 || self.theta == 360 {
+            // vertical line
+            edge_1_x = self.rho
+            edge_1_y = 0
+
+            Log.e("SPECIAL CASE NOT HANDLED")
+            return nil
+            // y_intercept not defined
+        } else if self.theta < 90 {
+            // search for x as hypotenuse 
+            let opposite = tan(self.theta*Double.pi/180)*self.rho
+            edge_1_x = sqrt(opposite*opposite + self.rho*self.rho)
+            edge_1_y = 0
+
+            y_intercept = self.rho / sin(self.theta*Double.pi/180)
+        } else if self.theta == 90 {
+            // flat horizontal line
+            edge_1_x = 0
+            edge_1_y = self.rho
+
+            y_intercept = self.rho
+            
+        } else if self.theta < 180 {
+            let opposite = tan((self.theta-90)*Double.pi/180)*self.rho
+            edge_1_x = 0
+            edge_1_y = sqrt(opposite*opposite + self.rho*self.rho)
+
+            y_intercept = self.rho / cos((self.theta-90)*Double.pi/180)
+        } else if self.theta < 270 {
+            Log.e("WTF, theta \(self.theta)?")
+
+            // these lines can't draw into the image unless rho is negative
+            // and that is accounted for elsewhere by reversing theta 180,
+            // and inverting rho
+            
+            Log.e("SPECIAL CASE NOT HANDLED")
+            return nil
+        } else if self.theta < 360 {
+            let opposite = tan((360-self.theta)*Double.pi/180)*self.rho
+            edge_1_x = sqrt(opposite*opposite + self.rho*self.rho)
+            edge_1_y = 0
+
+            y_intercept = -(self.rho / cos((self.theta-270)*Double.pi/180))
+        }
+
+        let rise_over_run = (edge_1_y - line_intersection_y) / (edge_1_x - line_intersection_x)
+
+        //Log.d("for theta \(self.theta) rho \(self.rho) intersection [\(line_intersection_x), \(line_intersection_y)], edge 1 [\(edge_1_x), \(edge_1_y)] rise over run \(rise_over_run) y_intercept \(y_intercept)")
+        
+        return CartesianLine(m: rise_over_run, c: y_intercept)
+    }
+}
+
+public struct CartesianLine {
+    public let m: Double                // rise over run
+    public let c: Double                // y intercept
+
+    public func y(for x: Int) -> Int {
+        // y = m*x + c
+        Int(m*Double(x) + c)
+    }
 }
 
 public enum Edge {
