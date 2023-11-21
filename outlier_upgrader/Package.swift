c0e5e6759258b5eacce1cdebeb916134409dbf7d
Brian Martin <brian.beholden@gmail.com>
2023-11-20 18:48:46 -0800

    update OutlierUpgrader to macos 13 for regex support use regexes to allow mirroring the same name format as the input sequence

--- outlier_upgrader/Package.swift
+++ outlier_upgrader/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "outlier_upgrader",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v13)
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
