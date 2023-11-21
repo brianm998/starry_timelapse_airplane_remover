e2552f8dbb555de1c2f5742be107193bb97070b9
Brian Martin <brian.beholden@gmail.com>
2023-11-20 18:11:39 -0800

    get the outlier upgrader writing out 16 bit tiff files

--- outlier_upgrader/Sources/OutlierUpgraderCLI.swift
+++ outlier_upgrader/Sources/OutlierUpgraderCLI.swift
@@ -37,9 +37,14 @@ You should have received a copy of the GNU General Public License along with sta
 struct OutlierUpgraderCLI: AsyncParsableCommand {
 
     @Option(name: [.short, .customLong("input-sequence-dir")], help:"""
-        Input file to blob
+        Input dir for outlier groups for a sequence
         """)
     var inputSequenceDir: String
+
+    @Option(name: [.short, .customLong("output-dir")], help:"""
+        Output dir for written files
+        """)
+    var outputDir: String
     
     mutating func run() async throws {
 
@@ -47,43 +52,114 @@ struct OutlierUpgraderCLI: AsyncParsableCommand {
 
         Log.i("Hello, Cruel World")
 
-        IMAGE_WIDTH = 6000      // XXX XXX XXX
-        IMAGE_HEIGHT = 4000     // XXX XXX XXX
-        
-        let frame_dirs = try fileManager.contentsOfDirectory(atPath: inputSequenceDir)
-        for item in frame_dirs {
-            if let frameIndex = Int(item),
-               let outlierGroups = try await loadOutliers(from: "\(inputSequenceDir)/\(frameIndex)", frameIndex: frameIndex)
-            {
-                Log.d("got frame \(frameIndex) w/ \(outlierGroups.members.count) outliers")
+        try await getImageSize()
+        try await writeOutlierValidationImage()
+    }
+
+    func writeOutlierValidationImage() async throws {
+        try await loadAllOutliers() { frameIndex, outlierGroups in
+            Log.d("got frame \(frameIndex) w/ \(outlierGroups.members.count) outliers")
+
+            // create base image data array
+            var baseData = [UInt16](repeating: 0, count: Int(IMAGE_WIDTH!*IMAGE_HEIGHT!))
+
+            // write into this array from the pixels in this group
+            for (groupName, group) in outlierGroups.members {
+                for x in 0 ..< group.bounds.width {
+                    for y in 0 ..< group.bounds.height {
+                        if group.pixels[y*group.bounds.width+x] != 0 {
+                            let imageX = x + group.bounds.min.x
+                            let imageY = y + group.bounds.min.y
+                            baseData[imageY*Int(IMAGE_WIDTH!)+imageX] = 0xFFFF
+                        }
+                    }
+                }
+            }
+
+            let outputFilename = "\(outputDir)/IMG_\(String(format: "%05d", frameIndex)).tiff"
+            do {
+                try save16BitMonoImageData(baseData, to: outputFilename)
+                Log.d("wrote \(outputFilename)")
+            } catch {
+                Log.e("could not write \(outputFilename): \(error)")
             }
         }
+    }
+
+    func getImageSize() async throws {
+        if let range: Range<String.Index> = inputSequenceDir.range(of: "-star-v-") {
+            let index = inputSequenceDir.distance(from: inputSequenceDir.startIndex,
+                                                  to: range.lowerBound)
+            let base_dir = String(inputSequenceDir[inputSequenceDir.startIndex..<range.lowerBound])
+            
+            let base_images = try fileManager.contentsOfDirectory(atPath: base_dir)
+            for imageName in base_images {
+                if imageName.hasSuffix(".tif") || imageName.hasSuffix(".tiff") {
+
+                    if let image = try await PixelatedImage(fromFile: "\(base_dir)/\(imageName)") {
+                        IMAGE_WIDTH = Double(image.width)
+                        IMAGE_HEIGHT = Double(image.height)
+                        break
+                    }
+                }
+            }
+        } else {
+            throw "cannot get base dir"
+        }
+
+        if let IMAGE_WIDTH = IMAGE_WIDTH,
+           let IMAGE_HEIGHT = IMAGE_HEIGHT
+        {
+            Log.i("got image size [\(IMAGE_WIDTH), \(IMAGE_HEIGHT)]")
+        } else {
+            fatalError("cannot read image size")
+        }
+    }
+    
+    func loadAllOutliers(_ closure: @escaping (Int, OutlierGroups) -> Void) async throws {
+        let frame_dirs = try fileManager.contentsOfDirectory(atPath: inputSequenceDir)
+
+        try await withLimitedThrowingTaskGroup(of: Void.self) { taskGroup in
+            for item in frame_dirs {
+                try await taskGroup.addTask() {
+                    if let frameIndex = Int(item),
+                       let outlierGroups = try await loadOutliers(from: "\(inputSequenceDir)/\(frameIndex)", frameIndex: frameIndex)
+                    {
+                        closure(frameIndex, outlierGroups)
+                    }
+                }
+            }
+        }
+    }
+
+    private func save16BitMonoImageData(_ inputPixels: [UInt16],
+                                        to filename: String) throws -> PixelatedImage
+    {
+        let imageData = inputPixels.withUnsafeBufferPointer { Data(buffer: $0) }
+
+        let image = PixelatedImage(width: Int(IMAGE_WIDTH!),
+                                   height: Int(IMAGE_HEIGHT!),
+                                   rawImageData: imageData,
+                                   bitsPerPixel: 16,
+                                   bytesPerRow: 2*Int(IMAGE_WIDTH!),
+                                   bitsPerComponent: 16,
+                                   bytesPerPixel: 2,
+                                   bitmapInfo: .byteOrder16Little, 
+                                   pixelOffset: 0,
+                                   colorSpace: CGColorSpaceCreateDeviceGray(),
+                                   ciFormat: .L16)
+        
+        try image.writeTIFFEncoding(toFilename: filename)
+
+        return image
     }
 }
 
 func loadOutliers(from dirname: String, frameIndex: Int) async throws -> OutlierGroups? {
-    var outlierGroupsForThisFrame: OutlierGroups?
-    
-    let startTime = Date().timeIntervalSinceReferenceDate
-    var endTime1: Double = 0
-    var startTime1: Double = 0
-    
     if FileManager.default.fileExists(atPath: dirname) {
-        startTime1 = Date().timeIntervalSinceReferenceDate
-        outlierGroupsForThisFrame = try await OutlierGroups(at: frameIndex, from: dirname)
-        endTime1 = Date().timeIntervalSinceReferenceDate
-        Log.i("frame \(frameIndex) loaded from new binary dir")
+        return try await OutlierGroups(at: frameIndex, from: dirname)
     } 
-    let end_time = Date().timeIntervalSinceReferenceDate
-    Log.d("took \(end_time - startTime) seconds to load outlier group data for frame \(frameIndex)")
-    Log.i("TIMES \(startTime1 - startTime) - \(endTime1 - startTime1) - \(end_time - endTime1) reading outlier group data for frame \(frameIndex)")
-    
-    if let _ = outlierGroupsForThisFrame  {
-        Log.i("loading frame \(frameIndex) with outlier groups from file")
-    } else {
-        Log.d("loading frame \(frameIndex)")
-    }
-    return outlierGroupsForThisFrame
+    return nil
 }
 
 fileprivate let fileManager = FileManager.default
