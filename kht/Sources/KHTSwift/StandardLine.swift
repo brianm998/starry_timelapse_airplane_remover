1399b9cd60f1ce8904826a37b5feb3cb135bdb3d
Brian Martin <brian.beholden@gmail.com>
2023-12-19 18:54:13 -0800

    add polarLine computed prop to StandardLine

--- kht/Sources/KHTSwift/StandardLine.swift
+++ kht/Sources/KHTSwift/StandardLine.swift
@@ -8,7 +8,63 @@ public struct StandardLine {
     let b: Double
     let c: Double
 
-    func intersection(with otherLine: StandardLine) -> DoubleCoord {
+    public init(a: Double,
+                b: Double,
+                c: Double)
+    {
+        self.a = a
+        self.b = b
+        self.c = c
+    }
+
+    // gives a line with polar coordinates with origin at [0, 0]
+    public var polarLine: Line {
+        let x_intercept = DoubleCoord(x: 0, y: self.y(forX: 0))
+        let y_intercept = DoubleCoord(x: self.x(forY: 0), y: 0)
+
+        if x_intercept.isFinite {
+            if y_intercept.isFinite {
+                // this line intersects both y and x axes
+                return Line(point1: x_intercept,
+                            point2: y_intercept)
+            } else {
+                // no Y intercept, but intercepts x, this is a vertical line
+                return Line(point1: x_intercept,
+                            point2: DoubleCoord(x: 10, y: self.y(forX: 10)))
+            }
+        } else {
+            if y_intercept.isFinite {
+                // no X intercept, but intercepts y, this is a horizontal line
+                return Line(point1: DoubleCoord(x: self.x(forY: 10), y: 10),
+                            point2: y_intercept)
+            } else {
+                // this line doesn't intersect either access? WTF
+                fatalError("somehow this line a = \(a) b = \(b) c = \(c) does not cross either the y or x axes")
+            }
+        }
+    }
+    
+    public init(point1: DoubleCoord, point2: DoubleCoord) {
+        self = point1.standardLine(with: point2)
+    }
+
+    public func y(forX x: Double) -> Double {
+        // a*x + b*y + c = 0
+        // a*x + b*y = -c
+        // b*y = -c - a*x 
+        // y = (-c - a*x)/b
+        return (-c - a*x)/b
+    }
+
+    public func x(forY y: Double) -> Double {
+        // a*x + b*y + c = 0
+        // a*x + b*y = -c
+        // a*x = -c - b*y
+        // x = (-c - b*y)/a
+        return (-c - b*y)/a
+    }
+    
+    public func intersection(with otherLine: StandardLine) -> DoubleCoord {
 
         let a1 = self.a
         let b1 = self.b
