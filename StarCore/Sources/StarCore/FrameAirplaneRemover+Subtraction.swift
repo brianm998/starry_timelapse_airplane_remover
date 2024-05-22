ae2c77e9b469c2f1e6ba04228e94383f4c8de257
Brian Martin <brian.beholden@gmail.com>
2024-05-22 06:52:48 -0700

    small refactor to subtraction logic

--- StarCore/Sources/StarCore/FrameAirplaneRemover+Subtraction.swift
+++ StarCore/Sources/StarCore/FrameAirplaneRemover+Subtraction.swift
@@ -25,32 +25,45 @@ extension FrameAirplaneRemover {
     // returns a grayscale image pixel value array from subtracting the aligned frame
     // from the frame being processed.
     internal func subtractAlignedImageFromFrame() async throws -> PixelatedImage {
+        Log.d("frame \(frameIndex) subtractAlignedImageFromFrame")
         
-        if let image = await imageAccessor.load(type: .original, atSize: .original),
-           let otherFrame = await imageAccessor.load(type: .aligned, atSize: .original)
-        {
-            self.state = .subtractingNeighbor
-            
-            Log.i("frame \(frameIndex) finding outliers")
-
-            let subtractionImage = image.subtract(otherFrame)
-            
-            if config.writeOutlierGroupFiles {
-                // write out image of outlier amounts
-                do {
-                    try await imageAccessor.save(subtractionImage, as: .subtracted,
-                                                 atSize: .original, overwrite: false)
-                    try await imageAccessor.save(subtractionImage, as: .subtracted,
-                                                 atSize: .preview, overwrite: false)
-                } catch {
-                    Log.e("can't write subtraction image: \(error)")
-                }
-            }
-
-            return subtractionImage
-            
-        } else {
-            throw "couldn't load images"
+        guard let image = await imageAccessor.load(type: .original, atSize: .original)
+        else {
+            Log.e("frame \(frameIndex) couldn't load original image")
+            // XXX these should really throw an error, and that really should
+            // be handled properly at a higher level, but right now, thrown errors
+            // from here end up in the bitbucket :(  Need to figure out why
+            throw "frame \(frameIndex) couldn't original image"
         }
+        Log.d("frame \(frameIndex) got orig image")
+        
+        guard let otherFrame = await imageAccessor.load(type: .aligned, atSize: .original)
+        else {
+            // XXX maybe try creating it here instead of dying??
+            Log.e("frame \(frameIndex) couldn't load aligned images")
+            throw "frame \(frameIndex) couldn't load aligned images"
+        }
+
+        Log.d("frame \(frameIndex) got aligned image")
+        
+        self.state = .subtractingNeighbor
+        
+        Log.i("frame \(frameIndex) finding outliers")
+
+        let subtractionImage = image.subtract(otherFrame)
+        
+        if config.writeOutlierGroupFiles {
+            // write out image of outlier amounts
+            do {
+                try await imageAccessor.save(subtractionImage, as: .subtracted,
+                                             atSize: .original, overwrite: false)
+                try await imageAccessor.save(subtractionImage, as: .subtracted,
+                                             atSize: .preview, overwrite: false)
+            } catch {
+                Log.e("can't write subtraction image: \(error)")
+            }
+        }
+
+        return subtractionImage
     }
 }
