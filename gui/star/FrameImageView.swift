bb2e32662ed83e93365ac2280e01f9e34b421ca6
Brian Martin <brian.beholden@gmail.com>
2024-05-26 09:37:10 -0700

    use onChange to load outliers when the frame changes

--- gui/star/FrameImageView.swift
+++ gui/star/FrameImageView.swift
@@ -1,7 +1,9 @@
 import SwiftUI
 import StarCore
 
-// displays a single frame as an image, of one of many different types
+// displays a single frame as an image, and nothing else.
+// the image may be preview, or full resolution,
+// and may be one of many different types (original, processed, etc)
 
 public struct FrameImageView: View {
     @EnvironmentObject var viewModel: ViewModel
@@ -57,6 +59,29 @@ public struct FrameImageView: View {
             return frameView.processedPreviewImage
         }
     }
+
+    private func maybeLoadOutliers() {
+        if interactionMode == .edit {
+            // try loading outliers if there aren't any present
+            let frameView = self.viewModel.frames[self.viewModel.currentIndex]
+
+            if frameView.outlierViews == nil,
+               !frameView.loadingOutlierViews,
+               let frame = frameView.frame
+            {
+                frameView.loadingOutlierViews = true
+                viewModel.loadingOutliers = true
+                Task.detached(priority: .userInitiated) {
+                    let _ = try await frame.loadOutliers()
+                    await self.viewModel.setOutlierGroups(forFrame: frame)
+                    Task { @MainActor in
+                        frameView.loadingOutlierViews = false
+                        self.viewModel.loadingOutliers = self.viewModel.loadingOutlierGroups
+                    }
+                }
+            }
+        } 
+    }
     
     public var body: some View {
         Group {
@@ -82,28 +107,8 @@ public struct FrameImageView: View {
                 }.opacity(viewModel.outlierOpacity)
             }
 
-        }.onAppear {
-            // XXX this only works the first time the ui shows, not when we transition frames :(
-            if interactionMode == .edit {
-                // try loading outliers if there aren't any present
-                let frameView = self.viewModel.frames[self.viewModel.currentIndex]
-
-                if frameView.outlierViews == nil,
-                   !frameView.loadingOutlierViews,
-                   let frame = frameView.frame
-                {
-                    frameView.loadingOutlierViews = true
-                    viewModel.loadingOutliers = true
-                    Task.detached(priority: .userInitiated) {
-                        let _ = try await frame.loadOutliers()
-                        await self.viewModel.setOutlierGroups(forFrame: frame)
-                        Task { @MainActor in
-                            frameView.loadingOutlierViews = false
-                            self.viewModel.loadingOutliers = self.viewModel.loadingOutlierGroups
-                        }
-                    }
-                }
-            } 
+        }.onChange(of: viewModel.currentIndex, initial: true) {
+            maybeLoadOutliers()
         }
     }
 }
