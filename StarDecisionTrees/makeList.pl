2dae039a145fc810f7e1b059c1ea698c2f553206
Brian Martin <brian.beholden@gmail.com>
2024-05-31 21:12:39 -0700

    add makeList.pl, which generates a swift file with a map of current decision trees

---
+++ StarDecisionTrees/makeList.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+use strict;
+
+# write a swift file listing all current decision trees in a map
+
+my $trees;
+
+opendir my $dir, 'Sources/StarDecisionTrees' or die "cannot open dir: $!\n";
+foreach my $filename (readdir $dir) {
+  # filename looks like OutlierGroupDecisionTreeForest_ec60776b.swift
+  if($filename =~ /_([a-f\d]+)[.]swift$/ && $filename !~ /#/)  {
+    my $hash = $1;
+    $filename =~ s/[.]swift$//;
+    $trees->{$hash} = $filename;
+  }
+}
+closedir $dir;
+
+open OUTPUT, ">Sources/StarDecisionTrees/StarDecisionTrees.swift";
+
+print OUTPUT "import Foundation\n\n";
+print OUTPUT "public let decisionTrees = [\n";
+foreach my $hash (keys %$trees) {
+  print OUTPUT "    \"$hash\": $trees->{$hash}(),\n";
+}
+print OUTPUT "]\n";
+
+close OUTPUT;
