1d6a6cea727c7f03bab329cdfb334ce5d089a2a3
Brian Martin <brian.beholden@gmail.com>
2024-06-01 17:18:30 -0700

    update decision tree build scripts

--- StarDecisionTrees/build_lib.sh
+++ StarDecisionTrees/build_lib.sh
@@ -1,19 +1,18 @@
 # re-build the categorizers as static .a files to avoid recompiling them
+# similar to release.sh, however builds on one arch and in debug mode, but still speed optimized
 
 # clean previous builds
 rm -rf .build
 
+# generate current list of all decision trees in StarDecisionTrees.swift
+./makeList.pl
+
 # first run after cleaning build file dies on error about missing objc bridging header for semaphore
 # re - run it without deleting the build file, and it works.
 
-# this one will fail
-swift build --arch `uname -m` -Xswiftc -emit-module -Xswiftc -emit-library -Xswiftc -static -Xswiftc -O
-
-# this one should work
 # build for current arch (development)
-swift build --arch `uname -m` -Xswiftc -emit-module -Xswiftc -emit-library -Xswiftc -static -Xswiftc -O
+swift build || swift build --arch `uname -m` -Xswiftc -emit-module -Xswiftc -emit-library -Xswiftc -static -Xswiftc -O
 
 # can't properly build against this with these still here
 rm -rf .build/debug/*.build
 
-./makeList.pl
--- StarDecisionTrees/release.sh
+++ StarDecisionTrees/release.sh
@@ -4,17 +4,30 @@ set -e
 
 ####
 # build the decision tree code into a static, universal library (.a file)
-# this can be large, and is linked into both the gui and cli apps
+# this can be large, and is linked into the gui and cli apps, as into the
+# decision tree generator.
 ####
 
 rm -rf .build
 
+# generate current list of all decision trees in StarDecisionTrees.swift
+./makeList.pl
+
 # build static, speed optimized lib for x86
-swift build --configuration release --arch x86_64 -Xswiftc -emit-module -Xswiftc -emit-library -Xswiftc -static -Xswiftc -O
+
+STATIC_X86_BUILD="swift build --configuration release --arch x86_64 -Xswiftc -emit-module -Xswiftc -emit-library -Xswiftc -static -Xswiftc -O"
+
+# run it like this incase the first one dies because of missing objc header 
+$STATIC_X86_BUILD || $STATIC_X86_BUILD
+
 mv *.a .build/x86_64-apple-macosx
 
 # build static, speed optimized lib for arm64
-swift build --configuration release --arch arm64  -Xswiftc -emit-module -Xswiftc -emit-library -Xswiftc -static -Xswiftc -O 
+
+STATIC_ARM_BUILD="swift build --configuration release --arch arm64  -Xswiftc -emit-module -Xswiftc -emit-library -Xswiftc -static -Xswiftc -O"
+
+$STATIC_ARM_BUILD || $STATIC_ARM_BUILD
+
 mv *.a .build/arm64-apple-macosx
 
 # lipo them together into a universal .a file for both archs
@@ -26,5 +39,3 @@ lipo .build/arm64-apple-macosx/libStarDecisionTrees.a \
 rm -rf .build/debug/*.build
 rm -rf .build/release/*.build
 
-# generate current list of all decision trees in StarDecisionTrees.swift
-./makeList.pl
