d1a4adf61fe3d2c30a0b34e3530a269a24d72e0d
Brian Martin <brian.beholden@gmail.com>
2024-06-16 11:32:48 -0700

    get the decision trees .a file working better

--- StarDecisionTrees/Package.swift
+++ StarDecisionTrees/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
           name: "StarDecisionTrees",
-          type: .dynamic,
+          type: .static,
           targets: ["StarDecisionTrees"]
         ),
     ],
