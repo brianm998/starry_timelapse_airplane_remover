11645843f4de2b9c6ae1e0fee1a9e95ceab0af2d
Brian Martin <brian.beholden@gmail.com>
2024-06-16 14:36:29 -0700

    add more comments to release script

--- StarDecisionTrees/release.sh
+++ StarDecisionTrees/release.sh
@@ -5,7 +5,15 @@ set -e
 ####
 # build the decision tree code into a static, universal library (.a file)
 # this can be large, and is linked into the gui and cli apps, as into the
-# decision tree generator.  This can take hours on a large set of trees.
+# decision tree generator.  It can take hours to build a large set of trees,
+# due to a number of factors:
+#  - decision trees are REALLY big swift files
+#  - compling with optimization on is slow
+#  - we have to compile the same thing three times here:
+#    1. get universal swift module definition
+#    2. get x86 .a file
+#    3. get arm .a file
+#  building both archs at once only gives us a .o file, fixing that would speed this up 3x
 ####
 
 # clear out any previous build
