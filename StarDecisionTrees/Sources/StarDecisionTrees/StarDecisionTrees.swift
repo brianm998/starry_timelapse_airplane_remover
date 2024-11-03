24ab588cc62283cfbd21107c1d6db0a57590fb11
Brian Martin <brian.beholden@gmail.com>
2024-11-03 10:48:10 -0800

    add cached only fetching to avoid crashes

--- StarDecisionTrees/Sources/StarDecisionTrees/StarDecisionTrees.swift
+++ StarDecisionTrees/Sources/StarDecisionTrees/StarDecisionTrees.swift
@@ -2,13 +2,22 @@ import Foundation
 import StarCore
 
 public let decisionTrees: [String: NamedOutlierGroupClassifier] = [
-    "457016e3": OutlierGroupDecisionTreeForest_457016e3(),
-    "5caedbc8": OutlierGroupDecisionTreeForest_5caedbc8(),
+    "336697a3": OutlierGroupDecisionTreeForest_336697a3(),
     "13227c6d": OutlierGroupForestClassifier_13227c6d(),
-    "7feafdde": OutlierGroupDecisionTreeForest_7feafdde(),
-    "bc60eef4": OutlierGroupDecisionTreeForest_bc60eef4(),
+    "405ad829": OutlierGroupDecisionTreeForest_405ad829(),
     "56404fe8": OutlierGroupDecisionTreeForest_56404fe8(),
-    "820063d2": OutlierGroupDecisionTreeForest_820063d2(),
-    "205093fd": OutlierGroupDecisionTreeForest_205093fd(),
+    "7feafdde": OutlierGroupDecisionTreeForest_7feafdde(),
+    "457016e3": OutlierGroupDecisionTreeForest_457016e3(),
+    "efe2cb87": OutlierGroupDecisionTreeForest_efe2cb87(),
+    "e52f3088": OutlierGroupForestClassifier_e52f3088(),
+    "8a484a2a": OutlierGroupDecisionTreeForest_8a484a2a(),
     "5ab1e186": OutlierGroupDecisionTreeForest_5ab1e186(),
+    "b166680b": OutlierGroupDecisionTreeForest_b166680b(),
+    "3e8979b3": OutlierGroupDecisionTreeForest_3e8979b3(),
+    "639b7349": OutlierGroupDecisionTreeForest_639b7349(),
+    "205093fd": OutlierGroupDecisionTreeForest_205093fd(),
+    "bc60eef4": OutlierGroupDecisionTreeForest_bc60eef4(),
+    "820063d2": OutlierGroupDecisionTreeForest_820063d2(),
+    "5f64564d": OutlierGroupDecisionTreeForest_5f64564d(),
+    "5caedbc8": OutlierGroupDecisionTreeForest_5caedbc8(),
 ]
--- gui/star/FrameViewModel.swift
+++ gui/star/FrameViewModel.swift
@@ -90,178 +90,221 @@ public class FrameViewModel {
 
     private var _alignedPreviewImage: Image?
 
-    var alignedPreviewImage: Image {
-        return lazyLoad(.aligned, storage: &_alignedPreviewImage) { image in
+    func alignedPreviewImage(cachedOnly: Bool = false) -> Image {
+        return lazyLoad(.aligned,
+                        cachedOnly: cachedOnly,
+                        storage: &_alignedPreviewImage) { image in
             self._alignedPreviewImage = image
         }
     }
 
     private var _subtractionPreviewImage: Image?
 
-    var subtractionPreviewImage: Image {
-        return lazyLoad(.subtraction, storage: &_subtractionPreviewImage) { image in
+    private func subtractionPreviewImage(cachedOnly: Bool = false) -> Image {
+        return lazyLoad(.subtraction,
+                        cachedOnly: cachedOnly,
+                        storage: &_subtractionPreviewImage) { image in
             self._subtractionPreviewImage = image
         }
     }
 
     private var _blobsPreviewImage: Image?
 
-    var blobsPreviewImage: Image {
-        return lazyLoad(.blobs, storage: &_blobsPreviewImage) { image in
+    private func blobsPreviewImage(cachedOnly: Bool = false) -> Image {
+        return lazyLoad(.blobs,
+                        cachedOnly: cachedOnly,
+                        storage: &_blobsPreviewImage) { image in
             self._blobsPreviewImage = image
         }
     }
 
     private var _filter1PreviewImage: Image?
 
-    var filter1PreviewImage: Image {
-        return lazyLoad(.filter1, storage: &_filter1PreviewImage) { image in
+    private func filter1PreviewImage(cachedOnly: Bool = false) -> Image {
+        return lazyLoad(.filter1,
+                        cachedOnly: cachedOnly,
+                        storage: &_filter1PreviewImage) { image in
             self._filter1PreviewImage = image
         }
     }
 
     private var _filter2PreviewImage: Image?
 
-    var filter2PreviewImage: Image {
-        return lazyLoad(.filter2, storage: &_filter2PreviewImage) { image in
+    private func filter2PreviewImage(cachedOnly: Bool = false) -> Image {
+        return lazyLoad(.filter2,
+                        cachedOnly: cachedOnly,
+                        storage: &_filter2PreviewImage) { image in
             self._filter2PreviewImage = image
         }
     }
 
     private var _filter3PreviewImage: Image?
 
-    var filter3PreviewImage: Image {
-        return lazyLoad(.filter3, storage: &_filter3PreviewImage) { image in
+    private func filter3PreviewImage(cachedOnly: Bool = false) -> Image {
+        return lazyLoad(.filter3,
+                        cachedOnly: cachedOnly,
+                        storage: &_filter3PreviewImage) { image in
             self._filter3PreviewImage = image
         }
     }
 
     private var _filter4PreviewImage: Image?
 
-    var filter4PreviewImage: Image {
-        return lazyLoad(.filter4, storage: &_filter4PreviewImage) { image in
+    private func filter4PreviewImage(cachedOnly: Bool = false) -> Image {
+        return lazyLoad(.filter4,
+                        cachedOnly: cachedOnly,
+                        storage: &_filter4PreviewImage) { image in
             self._filter4PreviewImage = image
         }
     }
 
     private var _filter5PreviewImage: Image?
 
-    var filter5PreviewImage: Image {
-        return lazyLoad(.filter5, storage: &_filter5PreviewImage) { image in
+    private func filter5PreviewImage(cachedOnly: Bool = false) -> Image {
+        return lazyLoad(.filter5,
+                        cachedOnly: cachedOnly,
+                        storage: &_filter5PreviewImage) { image in
             self._filter5PreviewImage = image
         }
     }
 
     private var _filter6PreviewImage: Image?
 
-    var filter6PreviewImage: Image {
-        return lazyLoad(.filter6, storage: &_filter6PreviewImage) { image in
+    private func filter6PreviewImage(cachedOnly: Bool = false) -> Image {
+        return lazyLoad(.filter6,
+                        cachedOnly: cachedOnly,
+                        storage: &_filter6PreviewImage) { image in
             self._filter6PreviewImage = image
         }
     }
 
     private var _filter7PreviewImage: Image?
 
-    var filter7PreviewImage: Image {
-        return lazyLoad(.filter7, storage: &_filter7PreviewImage) { image in
+    private func filter7PreviewImage(cachedOnly: Bool = false) -> Image {
+        return lazyLoad(.filter7,
+                        cachedOnly: cachedOnly,
+                        storage: &_filter7PreviewImage) { image in
             self._filter7PreviewImage = image
         }
     }
     
     private var _filter8PreviewImage: Image?
 
-    var filter8PreviewImage: Image {
-        return lazyLoad(.filter8, storage: &_filter8PreviewImage) { image in
+    private func filter8PreviewImage(cachedOnly: Bool = false) -> Image {
+        return lazyLoad(.filter8,
+                        cachedOnly: cachedOnly,
+                        storage: &_filter8PreviewImage) { image in
             self._filter8PreviewImage = image
         }
     }
 
     private var _filter9PreviewImage: Image?
 
-    var filter9PreviewImage: Image {
-        return lazyLoad(.filter9, storage: &_filter9PreviewImage) { image in
+    private func filter9PreviewImage(cachedOnly: Bool = false) -> Image {
+        return lazyLoad(.filter9,
+                        cachedOnly: cachedOnly,
+                        storage: &_filter9PreviewImage) { image in
             self._filter9PreviewImage = image
         }
     }
 
     private var _filter10PreviewImage: Image?
 
-    var filter10PreviewImage: Image {
-        return lazyLoad(.filter10, storage: &_filter10PreviewImage) { image in
+    private func filter10PreviewImage(cachedOnly: Bool = false) -> Image {
+        return lazyLoad(.filter10,
+                        cachedOnly: cachedOnly,
+                        storage: &_filter10PreviewImage) { image in
             self._filter10PreviewImage = image
         }
     }
     
     private var _filter11PreviewImage: Image?
 
-    var filter11PreviewImage: Image {
-        return lazyLoad(.filter11, storage: &_filter11PreviewImage) { image in
+    private func filter11PreviewImage(cachedOnly: Bool = false) -> Image {
+        return lazyLoad(.filter11,
+                        cachedOnly: cachedOnly,
+                        storage: &_filter11PreviewImage) { image in
             self._filter11PreviewImage = image
         }
     }
 
     private var _filter12PreviewImage: Image?
 
-    var filter12PreviewImage: Image {
-        return lazyLoad(.filter12, storage: &_filter12PreviewImage) { image in
+    private func filter12PreviewImage(cachedOnly: Bool = false) -> Image {
+        return lazyLoad(.filter12,
+                        cachedOnly: cachedOnly,
+                        storage: &_filter12PreviewImage) { image in
             self._filter12PreviewImage = image
         }
     }
 
     private var _filter13PreviewImage: Image?
 
-    var filter13PreviewImage: Image {
-        return lazyLoad(.filter13, storage: &_filter13PreviewImage) { image in
+    private func filter13PreviewImage(cachedOnly: Bool = false) -> Image {
+        return lazyLoad(.filter13,
+                        cachedOnly: cachedOnly,
+                        storage: &_filter13PreviewImage) { image in
             self._filter13PreviewImage = image
         }
     }
     
     private var _filter14PreviewImage: Image?
 
-    var filter14PreviewImage: Image {
-        return lazyLoad(.filter14, storage: &_filter14PreviewImage) { image in
+    private func filter14PreviewImage(cachedOnly: Bool = false) -> Image {
+        return lazyLoad(.filter14,
+                        cachedOnly: cachedOnly,
+                        storage: &_filter14PreviewImage) { image in
             self._filter14PreviewImage = image
         }
     }
 
     private var _filter15PreviewImage: Image?
 
-    var filter15PreviewImage: Image {
-        return lazyLoad(.filter15, storage: &_filter15PreviewImage) { image in
+    private func filter15PreviewImage(cachedOnly: Bool = false) -> Image {
+        return lazyLoad(.filter15,
+                        cachedOnly: cachedOnly,
+                        storage: &_filter15PreviewImage) { image in
             self._filter15PreviewImage = image
         }
     }
 
     private var _filter16PreviewImage: Image?
 
-    var filter16PreviewImage: Image {
-        return lazyLoad(.filter16, storage: &_filter16PreviewImage) { image in
+    private func filter16PreviewImage(cachedOnly: Bool = false) -> Image {
+        return lazyLoad(.filter16,
+                        cachedOnly: cachedOnly,
+                        storage: &_filter16PreviewImage) { image in
             self._filter16PreviewImage = image
         }
     }
 
     private var _paintMaskPreviewImage: Image?
 
-    var paintMaskPreviewImage: Image {
-        return lazyLoad(.paintMask, storage: &_paintMaskPreviewImage) { image in
+    private func paintMaskPreviewImage(cachedOnly: Bool = false) -> Image {
+        return lazyLoad(.paintMask,
+                        cachedOnly: cachedOnly,
+                        storage: &_paintMaskPreviewImage) { image in
             self._paintMaskPreviewImage = image
         }
     }
     
     private var _validationPreviewImage: Image?
 
-    var validationPreviewImage: Image {
-        return lazyLoad(.validation, storage: &_validationPreviewImage) { image in
+    private func validationPreviewImage(cachedOnly: Bool = false) -> Image { 
+        return lazyLoad(.validation,
+                        cachedOnly: cachedOnly,
+                        storage: &_validationPreviewImage) { image in
             self._validationPreviewImage = image
         }
     }
     
     private func lazyLoad(_ type: FrameViewMode,
+                          cachedOnly: Bool,
                           storage: inout Image?,
                           closure: @escaping (Image) -> Void) -> Image
     {
         if let storage { return storage }
-
+        if cachedOnly { return initialImage }
         if let frame {
             Task {
                 let bgTask = Task.detached {
@@ -269,66 +312,74 @@ public class FrameViewModel {
                                                   atSize: .preview)?.resizable()
                 }
                 if let image = await bgTask.value {
+//                    print("FUCKING image")
                     closure(image)
+                } else {
+//                    print("FUCKING FUCK")
+                    closure(initialImage)
                 }
             }
         }
 
         if viewModel.frameViewMode != viewModel.previousFrameViewMode {
-            return previewImage(type: viewModel.previousFrameViewMode)
+            // show previous image while loading if we can
+
+            // XXX show loading icon on top
+//            print("FUCKING viewModel.previousFrameViewMode \(viewModel.previousFrameViewMode)")
+            return previewImage(type: viewModel.previousFrameViewMode, cachedOnly: true)
         } else {
             return initialImage
         }
     }
     
-    public func previewImage(type: FrameViewMode) -> Image {
+    public func previewImage(type: FrameViewMode, cachedOnly: Bool = false) -> Image {
         switch type {
         case .original:
             return self.previewImage
-        case .subtraction:
-            return self.subtractionPreviewImage
-        case .blobs:
-            return self.blobsPreviewImage
-        case .filter1:
-            return self.filter1PreviewImage
-        case .filter2:
-            return self.filter2PreviewImage
-        case .filter3:
-            return self.filter3PreviewImage
-        case .filter4:
-            return self.filter4PreviewImage
-        case .filter5:
-            return self.filter5PreviewImage
-        case .filter6:
-            return self.filter6PreviewImage
-        case .filter7:
-            return self.filter7PreviewImage
-        case .filter8:
-            return self.filter8PreviewImage
-        case .filter9:
-            return self.filter9PreviewImage
-        case .filter10:
-            return self.filter10PreviewImage
-        case .filter11:
-            return self.filter11PreviewImage
-        case .filter12:
-            return self.filter12PreviewImage
-        case .filter13:
-            return self.filter13PreviewImage
-        case .filter14:
-            return self.filter14PreviewImage
-        case .filter15:
-            return self.filter15PreviewImage
-        case .filter16:
-            return self.filter16PreviewImage
-        case .paintMask:
-            return self.paintMaskPreviewImage
-        case .validation:
-            return self.validationPreviewImage
         case .processed:
             return self.processedPreviewImage
+        case .subtraction:
+            return self.subtractionPreviewImage(cachedOnly: cachedOnly)
+        case .blobs:
+            return self.blobsPreviewImage(cachedOnly: cachedOnly)
+        case .filter1:
+            return self.filter1PreviewImage(cachedOnly: cachedOnly)
+        case .filter2:
+            return self.filter2PreviewImage(cachedOnly: cachedOnly)
+        case .filter3:
+            return self.filter3PreviewImage(cachedOnly: cachedOnly)
+        case .filter4:
+            return self.filter4PreviewImage(cachedOnly: cachedOnly)
+        case .filter5:
+            return self.filter5PreviewImage(cachedOnly: cachedOnly)
+        case .filter6:
+            return self.filter6PreviewImage(cachedOnly: cachedOnly)
+        case .filter7:
+            return self.filter7PreviewImage(cachedOnly: cachedOnly)
+        case .filter8:
+            return self.filter8PreviewImage(cachedOnly: cachedOnly)
+        case .filter9:
+            return self.filter9PreviewImage(cachedOnly: cachedOnly)
+        case .filter10:
+            return self.filter10PreviewImage(cachedOnly: cachedOnly)
+        case .filter11:
+            return self.filter11PreviewImage(cachedOnly: cachedOnly)
+        case .filter12:
+            return self.filter12PreviewImage(cachedOnly: cachedOnly)
+        case .filter13:
+            return self.filter13PreviewImage(cachedOnly: cachedOnly)
+        case .filter14:
+            return self.filter14PreviewImage(cachedOnly: cachedOnly)
+        case .filter15:
+            return self.filter15PreviewImage(cachedOnly: cachedOnly)
+        case .filter16:
+            return self.filter16PreviewImage(cachedOnly: cachedOnly)
+        case .paintMask:
+            return self.paintMaskPreviewImage(cachedOnly: cachedOnly)
+        case .validation:
+            return self.validationPreviewImage(cachedOnly: cachedOnly)
         case .aligned:
-            return self.alignedPreviewImage
+            return self.alignedPreviewImage(cachedOnly: cachedOnly)
         }
     }
 
