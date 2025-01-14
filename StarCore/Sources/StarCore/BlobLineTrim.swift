722b986253b38a59814ec3bc66a262ff04d08ed6
Brian Martin <brian.beholden@gmail.com>
2025-01-13 21:17:11 -0800

    get the BlobLineTrim parallelization working better

--- StarCore/Sources/StarCore/BlobLineTrim.swift
+++ StarCore/Sources/StarCore/BlobLineTrim.swift
@@ -144,7 +144,11 @@ public actor BlobLineTrim {
         await withTaskGroup(of: [Blob].self) { taskGroup in
             for (_, blob) in blobMap {
                 taskGroup.addTask {
-                    await self.process(blob, with: args, maxIterations: 10)
+                    await StarCore.process(blob,
+                                           with: args,
+                                           maxBlobID: self.maxBlobID,
+                                           frameIndex: self.frameIndex,
+                                           maxIterations: 10)
                 }
 
                 for await newBlobs in taskGroup {
@@ -156,43 +160,53 @@ public actor BlobLineTrim {
         }
         return blobMap
     }
+}
 
-    // maybe break up the given blob into a set of other blobs, each of which is more linear
-    private func process(_ blob: Blob, with args: Args, maxIterations: Int) async -> [Blob] {
-        if await blob.size() > args.minBlobSize, // ignore small blobs
-           let lineLength = await blob.lineLength(), // must know the line length
-           lineLength > args.minLineLength,          // line length must be big enough
-           let lineFillAmount = await blob.blobLineScore(), // must know the line fill amount
-           lineFillAmount > args.minLineFillAmount    // line fill amount must be big enough
-        {
-            // trim that shit
-            let trimmedPixels = await blob.lineTrim(by: args.trimAmount)
-            if trimmedPixels.count > 0 {
-                let newBlobID = await maxBlobID.increment()
-                if newBlobID < UInt16.max {
+// maybe break up the given blob into a set of other blobs, each of which is more linear
+fileprivate func process(_ blob: Blob,
+                         with args: BlobLineTrim.Args,
+                         maxBlobID: IntegralActor,
+                         frameIndex: Int,
+                         maxIterations: Int) async -> [Blob]
+{
+    if await blob.size() > args.minBlobSize, // ignore small blobs
+       let lineLength = await blob.lineLength(), // must know the line length
+       lineLength > args.minLineLength,          // line length must be big enough
+       let lineFillAmount = await blob.blobLineScore(), // must know the line fill amount
+       lineFillAmount > args.minLineFillAmount    // line fill amount must be big enough
+    {
+        // trim that shit
+        let trimmedPixels = await blob.lineTrim(by: args.trimAmount)
+        if trimmedPixels.count > 0 {
+            let newBlobID = await maxBlobID.increment()
+            if newBlobID < UInt16.max {
 
-                    // iterate on this
-                    
-                    // make another blob from any trimmed pixels
-                    let newBlob = Blob(trimmedPixels,
-                                       id: UInt16(newBlobID),
-                                       frameIndex: frameIndex)
+                // iterate on this
+                
+                // make another blob from any trimmed pixels
+                let newBlob = Blob(trimmedPixels,
+                                   id: UInt16(newBlobID),
+                                   frameIndex: frameIndex)
 
-                    var ret = [newBlob]
-                    
-                    if maxIterations > 1 {
-                      await ret += process(newBlob, with: args, maxIterations: maxIterations-1)
-                    }
-
-                    return ret
-                } else {
-                    // avoid arithmetic overflow
-                    Log.w("frame \(frameIndex) breaking on blob line trim because max blob id \(maxBlobID) is == UInt16.max")
-                    // re-absorb the trimmed pixels into the same blob
-                    await blob.absorb(trimmedPixels)
+                var ret = [newBlob]
+                
+                if maxIterations > 1 {
+                    await ret += process(newBlob,
+                                         with: args,
+                                         maxBlobID: maxBlobID,
+                                         frameIndex: frameIndex,
+                                         maxIterations: maxIterations-1)
                 }
+
+                return ret
+            } else {
+                // avoid arithmetic overflow
+                Log.w("frame \(frameIndex) breaking on blob line trim because max blob id \(maxBlobID) is == UInt16.max")
+                // re-absorb the trimmed pixels into the same blob
+                await blob.absorb(trimmedPixels)
             }
         }
-        return []
     }
+    return []
 }
+
