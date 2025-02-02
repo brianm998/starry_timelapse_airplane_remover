3abe4caa69487dc2b35044cfc232917c91dcfc77
Brian Martin <brian.beholden@gmail.com>
2025-02-01 16:50:44 -0800

    put the dustbin behind the other outliers

--- gui/star/FrameEditImageView.swift
+++ gui/star/FrameEditImageView.swift
@@ -120,6 +120,14 @@ public struct FrameEditImageView: View {
                 ZStack() {
                     // in edit mode, show outliers groups
 
+                    // then the outliers that have view models
+                    if let outlierViews = frameView.outlierViews {
+                        ForEach(outlierViews) { outlierViewModel in
+                            OutlierGroupView(groupViewModel: outlierViewModel)
+                              .id(localID)
+                        }
+                    }
+
                     // dustbin does below
                     if self.viewModel.shouldShowDustbin,
                        let dustbinImage = frameView.dustbinImage
@@ -127,14 +135,6 @@ public struct FrameEditImageView: View {
                         dustbinImage
                           .renderingMode(.template) 
                           .foregroundColor(.yellow)
-                    }
-
-                    // then the outliers that have view models
-                    if let outlierViews = frameView.outlierViews {
-                        ForEach(outlierViews) { outlierViewModel in
-                            OutlierGroupView(groupViewModel: outlierViewModel)
-                              .id(localID)
-                        }
                     }
                 }.opacity(viewModel.outlierOpacity)
             }
