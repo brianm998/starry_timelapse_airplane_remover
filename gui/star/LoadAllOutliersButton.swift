3883ade36efe8aff87f5767d2ff9cb37a701aaae
Brian Martin <brian.beholden@gmail.com>
2025-07-03 15:39:35 -0700

    remove unnecessary do { } catch { }

--- gui/star/LoadAllOutliersButton.swift
+++ gui/star/LoadAllOutliersButton.swift
@@ -23,54 +23,52 @@ struct LoadAllOutliersButton: View {
 
             // XXX move this logic into the view model (or deeper)
             Task.detached {
-                do {
-                    let startTime = Date().timeIntervalSinceReferenceDate
-                    
-                    await withThrowingTaskGroup(of: Void.self) { taskGroup in
-                        Log.d("load all outliers button pressed 2x")
-                        //let max_concurrent = ProcessInfo.processInfo.activeProcessorCount
-                        // this gets "Too many open files" with more than 2000 images :(
-                        for frameView in await viewModel.frames {
-                            if loadingType == .fromCurrentFrame,
-                               await frameView.frameIndex < viewModel.currentIndex
-                            {
-                                Log.d("skipping loading outliers for frame \(frameView.frameIndex)")
-                                continue
-                            }
 
-                            //Log.d("frame \(frameView.frameIndex) attempting to load outliers")
-                            if let frame = await frameView.frame {
-                                //Log.d("frame \(frameView.frameIndex) adding task to load outliers")
-                                taskGroup.addTask(/*priority: .userInitiated*/) {
-                                    // XXX style the button during this flow?
-                                    //Log.d("actually loading outliers for frame \(frame.frameIndex)")
-                                    try await frame.loadOutliers()
-                                    // XXX set this in the view model
+                let startTime = Date().timeIntervalSinceReferenceDate
+                
+                await withThrowingTaskGroup(of: Void.self) { taskGroup in
+                    Log.d("load all outliers button pressed 2x")
+                    //let max_concurrent = ProcessInfo.processInfo.activeProcessorCount
+                    // this gets "Too many open files" with more than 2000 images :(
+                    for frameView in await viewModel.frames {
+                        if loadingType == .fromCurrentFrame,
+                           await frameView.frameIndex < viewModel.currentIndex
+                        {
+                            Log.d("skipping loading outliers for frame \(frameView.frameIndex)")
+                            continue
+                        }
 
-                                    Task { @MainActor in
-                                        viewModel.numberOfFramesWithOutliersLoaded += 1
-                                        await viewModel.setOutlierGroups(forFrame: frame)
-                                    }
+                        //Log.d("frame \(frameView.frameIndex) attempting to load outliers")
+                        if let frame = await frameView.frame {
+                            //Log.d("frame \(frameView.frameIndex) adding task to load outliers")
+                            taskGroup.addTask(/*priority: .userInitiated*/) {
+                                // XXX style the button during this flow?
+                                //Log.d("actually loading outliers for frame \(frame.frameIndex)")
+                                try await frame.loadOutliers()
+                                // XXX set this in the view model
+
+                                Task { @MainActor in
+                                    viewModel.numberOfFramesWithOutliersLoaded += 1
+                                    await viewModel.setOutlierGroups(forFrame: frame)
                                 }
-                            } else {
-                                Log.d("frame \(frameView.frameIndex) no frame, can't load outliers")
                             }
+                        } else {
+                            Log.d("frame \(frameView.frameIndex) no frame, can't load outliers")
                         }
-                        do {
-                            try await taskGroup.waitForAll()
-                        } catch {
-                            Log.e("\(error)")
-                        }
-                        
-                        let end_time = Date().timeIntervalSinceReferenceDate
-                        await MainActor.run {
-                            viewModel.loadingAllOutliers = false
-                        }
-                      Log.d("foobar loaded outliers for \(await viewModel.frames.count) frames in \(end_time - startTime) seconds")
-                    }                                 
-                } catch {
-                    Log.e("\(error)")
-                }
+                    }
+                    do {
+                        try await taskGroup.waitForAll()
+                    } catch {
+                        Log.e("\(error)")
+                    }
+                    
+                    let end_time = Date().timeIntervalSinceReferenceDate
+                    await MainActor.run {
+                        viewModel.loadingAllOutliers = false
+                    }
+                    Log.d("foobar loaded outliers for \(await viewModel.frames.count) frames in \(end_time - startTime) seconds")
+                }                                 
+
             }
         }
 
