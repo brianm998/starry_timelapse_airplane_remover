5513c7e3e2fd93c6939a9f8df73b71e4b2676053
Brian Martin <brian.beholden@gmail.com>
2025-07-06 13:35:46 -0700

    split out the BlobMapper and BlobMapping from the HoughLineMatrix processor

---
+++ StarCore/Sources/StarCore/BlobMapper.swift
@@ -0,0 +1,71 @@
+import Foundation
+import CoreGraphics
+import KHTSwift
+import logging
+import Cocoa
+
+/*
+
+This file is part of the Starry Timelapse Airplane Remover (star).
+
+star is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+star is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with star. If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+// keeps a set of BlobMappings, outputs a list of what mappings that are connected
+public class BlobMapper {
+    public var mappings: Set<BlobMapping>
+
+    public init() {
+        self.mappings = Set<BlobMapping>()
+    }
+
+    public init(mappings: Set<BlobMapping>) {
+        self.mappings = mappings
+    }
+
+    // outputs a list of lists of adjecent blobs
+    public var mappingLists: [[UInt32]] {
+        // 1) Build an adjacency list
+        var graph = [UInt32: Set<UInt32>]()
+        for m in mappings {
+            graph[m.id1, default: []].insert(m.id2)
+            graph[m.id2, default: []].insert(m.id1)
+        }
+
+        // 2) Track which nodes we’ve already visited
+        var visited = Set<UInt32>()
+        var groups: [[UInt32]] = []
+
+        // 3) For each node, if not visited, BFS/DFS to collect its component
+        for node in graph.keys {
+            guard !visited.contains(node) else { continue }
+            var stack = [node]
+            var component = [UInt32]()
+
+            visited.insert(node)
+            while let current = stack.popLast() {
+                component.append(current)
+                for neighbor in graph[current]! {
+                    if !visited.contains(neighbor) {
+                        visited.insert(neighbor)
+                        stack.append(neighbor)
+                    }
+                }
+            }
+
+            // Optionally sort each component for determinism
+            groups.append(component.sorted())
+        }
+
+        return groups
+    }
+    
+    public static func + (lhs: BlobMapper, rhs: BlobMapper) -> BlobMapper {
+        BlobMapper(mappings: lhs.mappings.union(rhs.mappings)) 
+    }
+}
---
+++ StarCore/Sources/StarCore/BlobMapping.swift
@@ -0,0 +1,43 @@
+import Foundation
+import logging
+
+/*
+
+This file is part of the Starry Timelapse Airplane Remover (star).
+
+star is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+star is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with star. If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+// indicates that two blobs are linked, i.e. mapped together
+public struct BlobMapping: Hashable, Equatable, Sendable {
+    let id1: UInt32
+    let id2: UInt32
+
+    public init(_ id1: UInt32, _ id2: UInt32) {
+        // make sure the ids are ordered so (2,1) == (1,2)
+        if id1 < id2 {
+            self.id1 = id1
+            self.id2 = id2
+        } else {
+            self.id1 = id2
+            self.id2 = id1
+        }
+    }
+    
+    public func contains(id: UInt32) -> Bool { id1 == id || id2 == id }
+    
+    public static func == (lhs: BlobMapping, rhs: BlobMapping) -> Bool {
+        // [a,b] == [a,b]
+        if lhs.id1 == rhs.id1,
+           lhs.id2 == rhs.id2
+        {
+            return true
+        }
+        return false
+    }
+}
