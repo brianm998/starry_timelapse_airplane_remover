7439b05e1cd6fcf80de53a14dfe551713d4345e3
Brian Martin <brian.beholden@gmail.com>
2025-07-06 18:05:57 -0700

    new decision trees, small data set this time

---
+++ StarDecisionTrees/Sources/StarDecisionTrees/OutlierGroupForestClassifier_f9f52500.swift
@@ -0,0 +1,91 @@
+/*
+   written by decision_tree_generator on 2025-07-07 00:25:24 +0000.
+
+   The classifications of 8 trees are combined here with weights from test data.
+   
+   Trees were computed to the maximum depth possible
+
+   tree(StarCore.DecisionTreeParams(name: "1c6ddc95", inputSequences: ["/qp/star_validated/7_6_2025-small-test-train"], positiveTrainingSize: 10281, negativeTrainingSize: 302790, decisionTypes: [StarCore.OutlierGroupFeature.size, StarCore.OutlierGroupFeature.width, StarCore.OutlierGroupFeature.height, StarCore.OutlierGroupFeature.centerX, StarCore.OutlierGroupFeature.centerY, StarCore.OutlierGroupFeature.minX, StarCore.OutlierGroupFeature.minY, StarCore.OutlierGroupFeature.maxX, StarCore.OutlierGroupFeature.maxY, StarCore.OutlierGroupFeature.hypotenuse, StarCore.OutlierGroupFeature.aspectRatio, StarCore.OutlierGroupFeature.fillAmount, StarCore.OutlierGroupFeature.surfaceAreaRatio, StarCore.OutlierGroupFeature.averagebrightness, StarCore.OutlierGroupFeature.medianBrightness, StarCore.OutlierGroupFeature.maxBrightness, StarCore.OutlierGroupFeature.maxHoughTransformCount, StarCore.OutlierGroupFeature.pixelBorderAmount, StarCore.OutlierGroupFeature.averageLineVariance, StarCore.OutlierGroupFeature.medianLineVariance, StarCore.OutlierGroupFeature.lineLength, StarCore.OutlierGroupFeature.lineIntensityScore, StarCore.OutlierGroupFeature.linePixelScore, StarCore.OutlierGroupFeature.bunchCount, StarCore.OutlierGroupFeature.medianBunchSize, StarCore.OutlierGroupFeature.maxBunchSize], decisionSplitTypes: [StarCore.DecisionSplitType.median], maxDepth: Optional(-1), pruned: false))
+
+   tree(StarCore.DecisionTreeParams(name: "4ced1c88", inputSequences: ["/qp/star_validated/7_6_2025-small-test-train"], positiveTrainingSize: 10281, negativeTrainingSize: 302790, decisionTypes: [StarCore.OutlierGroupFeature.size, StarCore.OutlierGroupFeature.width, StarCore.OutlierGroupFeature.height, StarCore.OutlierGroupFeature.centerX, StarCore.OutlierGroupFeature.centerY, StarCore.OutlierGroupFeature.minX, StarCore.OutlierGroupFeature.minY, StarCore.OutlierGroupFeature.maxX, StarCore.OutlierGroupFeature.maxY, StarCore.OutlierGroupFeature.hypotenuse, StarCore.OutlierGroupFeature.aspectRatio, StarCore.OutlierGroupFeature.fillAmount, StarCore.OutlierGroupFeature.surfaceAreaRatio, StarCore.OutlierGroupFeature.averagebrightness, StarCore.OutlierGroupFeature.medianBrightness, StarCore.OutlierGroupFeature.maxBrightness, StarCore.OutlierGroupFeature.maxHoughTransformCount, StarCore.OutlierGroupFeature.pixelBorderAmount, StarCore.OutlierGroupFeature.averageLineVariance, StarCore.OutlierGroupFeature.medianLineVariance, StarCore.OutlierGroupFeature.lineLength, StarCore.OutlierGroupFeature.lineIntensityScore, StarCore.OutlierGroupFeature.linePixelScore, StarCore.OutlierGroupFeature.bunchCount, StarCore.OutlierGroupFeature.medianBunchSize, StarCore.OutlierGroupFeature.maxBunchSize], decisionSplitTypes: [StarCore.DecisionSplitType.median], maxDepth: Optional(-1), pruned: false))
+
+   tree(StarCore.DecisionTreeParams(name: "427d0bd4", inputSequences: ["/qp/star_validated/7_6_2025-small-test-train"], positiveTrainingSize: 10281, negativeTrainingSize: 302790, decisionTypes: [StarCore.OutlierGroupFeature.size, StarCore.OutlierGroupFeature.width, StarCore.OutlierGroupFeature.height, StarCore.OutlierGroupFeature.centerX, StarCore.OutlierGroupFeature.centerY, StarCore.OutlierGroupFeature.minX, StarCore.OutlierGroupFeature.minY, StarCore.OutlierGroupFeature.maxX, StarCore.OutlierGroupFeature.maxY, StarCore.OutlierGroupFeature.hypotenuse, StarCore.OutlierGroupFeature.aspectRatio, StarCore.OutlierGroupFeature.fillAmount, StarCore.OutlierGroupFeature.surfaceAreaRatio, StarCore.OutlierGroupFeature.averagebrightness, StarCore.OutlierGroupFeature.medianBrightness, StarCore.OutlierGroupFeature.maxBrightness, StarCore.OutlierGroupFeature.maxHoughTransformCount, StarCore.OutlierGroupFeature.pixelBorderAmount, StarCore.OutlierGroupFeature.averageLineVariance, StarCore.OutlierGroupFeature.medianLineVariance, StarCore.OutlierGroupFeature.lineLength, StarCore.OutlierGroupFeature.lineIntensityScore, StarCore.OutlierGroupFeature.linePixelScore, StarCore.OutlierGroupFeature.bunchCount, StarCore.OutlierGroupFeature.medianBunchSize, StarCore.OutlierGroupFeature.maxBunchSize], decisionSplitTypes: [StarCore.DecisionSplitType.median], maxDepth: Optional(-1), pruned: false))
+
+   tree(StarCore.DecisionTreeParams(name: "73321d6c", inputSequences: ["/qp/star_validated/7_6_2025-small-test-train"], positiveTrainingSize: 10281, negativeTrainingSize: 302790, decisionTypes: [StarCore.OutlierGroupFeature.size, StarCore.OutlierGroupFeature.width, StarCore.OutlierGroupFeature.height, StarCore.OutlierGroupFeature.centerX, StarCore.OutlierGroupFeature.centerY, StarCore.OutlierGroupFeature.minX, StarCore.OutlierGroupFeature.minY, StarCore.OutlierGroupFeature.maxX, StarCore.OutlierGroupFeature.maxY, StarCore.OutlierGroupFeature.hypotenuse, StarCore.OutlierGroupFeature.aspectRatio, StarCore.OutlierGroupFeature.fillAmount, StarCore.OutlierGroupFeature.surfaceAreaRatio, StarCore.OutlierGroupFeature.averagebrightness, StarCore.OutlierGroupFeature.medianBrightness, StarCore.OutlierGroupFeature.maxBrightness, StarCore.OutlierGroupFeature.maxHoughTransformCount, StarCore.OutlierGroupFeature.pixelBorderAmount, StarCore.OutlierGroupFeature.averageLineVariance, StarCore.OutlierGroupFeature.medianLineVariance, StarCore.OutlierGroupFeature.lineLength, StarCore.OutlierGroupFeature.lineIntensityScore, StarCore.OutlierGroupFeature.linePixelScore, StarCore.OutlierGroupFeature.bunchCount, StarCore.OutlierGroupFeature.medianBunchSize, StarCore.OutlierGroupFeature.maxBunchSize], decisionSplitTypes: [StarCore.DecisionSplitType.median], maxDepth: Optional(-1), pruned: false))
+
+   tree(StarCore.DecisionTreeParams(name: "6fa1991d", inputSequences: ["/qp/star_validated/7_6_2025-small-test-train"], positiveTrainingSize: 10281, negativeTrainingSize: 302790, decisionTypes: [StarCore.OutlierGroupFeature.size, StarCore.OutlierGroupFeature.width, StarCore.OutlierGroupFeature.height, StarCore.OutlierGroupFeature.centerX, StarCore.OutlierGroupFeature.centerY, StarCore.OutlierGroupFeature.minX, StarCore.OutlierGroupFeature.minY, StarCore.OutlierGroupFeature.maxX, StarCore.OutlierGroupFeature.maxY, StarCore.OutlierGroupFeature.hypotenuse, StarCore.OutlierGroupFeature.aspectRatio, StarCore.OutlierGroupFeature.fillAmount, StarCore.OutlierGroupFeature.surfaceAreaRatio, StarCore.OutlierGroupFeature.averagebrightness, StarCore.OutlierGroupFeature.medianBrightness, StarCore.OutlierGroupFeature.maxBrightness, StarCore.OutlierGroupFeature.maxHoughTransformCount, StarCore.OutlierGroupFeature.pixelBorderAmount, StarCore.OutlierGroupFeature.averageLineVariance, StarCore.OutlierGroupFeature.medianLineVariance, StarCore.OutlierGroupFeature.lineLength, StarCore.OutlierGroupFeature.lineIntensityScore, StarCore.OutlierGroupFeature.linePixelScore, StarCore.OutlierGroupFeature.bunchCount, StarCore.OutlierGroupFeature.medianBunchSize, StarCore.OutlierGroupFeature.maxBunchSize], decisionSplitTypes: [StarCore.DecisionSplitType.median], maxDepth: Optional(-1), pruned: false))
+
+   tree(StarCore.DecisionTreeParams(name: "366885cb", inputSequences: ["/qp/star_validated/7_6_2025-small-test-train"], positiveTrainingSize: 10281, negativeTrainingSize: 302790, decisionTypes: [StarCore.OutlierGroupFeature.size, StarCore.OutlierGroupFeature.width, StarCore.OutlierGroupFeature.height, StarCore.OutlierGroupFeature.centerX, StarCore.OutlierGroupFeature.centerY, StarCore.OutlierGroupFeature.minX, StarCore.OutlierGroupFeature.minY, StarCore.OutlierGroupFeature.maxX, StarCore.OutlierGroupFeature.maxY, StarCore.OutlierGroupFeature.hypotenuse, StarCore.OutlierGroupFeature.aspectRatio, StarCore.OutlierGroupFeature.fillAmount, StarCore.OutlierGroupFeature.surfaceAreaRatio, StarCore.OutlierGroupFeature.averagebrightness, StarCore.OutlierGroupFeature.medianBrightness, StarCore.OutlierGroupFeature.maxBrightness, StarCore.OutlierGroupFeature.maxHoughTransformCount, StarCore.OutlierGroupFeature.pixelBorderAmount, StarCore.OutlierGroupFeature.averageLineVariance, StarCore.OutlierGroupFeature.medianLineVariance, StarCore.OutlierGroupFeature.lineLength, StarCore.OutlierGroupFeature.lineIntensityScore, StarCore.OutlierGroupFeature.linePixelScore, StarCore.OutlierGroupFeature.bunchCount, StarCore.OutlierGroupFeature.medianBunchSize, StarCore.OutlierGroupFeature.maxBunchSize], decisionSplitTypes: [StarCore.DecisionSplitType.median], maxDepth: Optional(-1), pruned: false))
+
+   tree(StarCore.DecisionTreeParams(name: "44adcedc", inputSequences: ["/qp/star_validated/7_6_2025-small-test-train"], positiveTrainingSize: 10282, negativeTrainingSize: 302791, decisionTypes: [StarCore.OutlierGroupFeature.size, StarCore.OutlierGroupFeature.width, StarCore.OutlierGroupFeature.height, StarCore.OutlierGroupFeature.centerX, StarCore.OutlierGroupFeature.centerY, StarCore.OutlierGroupFeature.minX, StarCore.OutlierGroupFeature.minY, StarCore.OutlierGroupFeature.maxX, StarCore.OutlierGroupFeature.maxY, StarCore.OutlierGroupFeature.hypotenuse, StarCore.OutlierGroupFeature.aspectRatio, StarCore.OutlierGroupFeature.fillAmount, StarCore.OutlierGroupFeature.surfaceAreaRatio, StarCore.OutlierGroupFeature.averagebrightness, StarCore.OutlierGroupFeature.medianBrightness, StarCore.OutlierGroupFeature.maxBrightness, StarCore.OutlierGroupFeature.maxHoughTransformCount, StarCore.OutlierGroupFeature.pixelBorderAmount, StarCore.OutlierGroupFeature.averageLineVariance, StarCore.OutlierGroupFeature.medianLineVariance, StarCore.OutlierGroupFeature.lineLength, StarCore.OutlierGroupFeature.lineIntensityScore, StarCore.OutlierGroupFeature.linePixelScore, StarCore.OutlierGroupFeature.bunchCount, StarCore.OutlierGroupFeature.medianBunchSize, StarCore.OutlierGroupFeature.maxBunchSize], decisionSplitTypes: [StarCore.DecisionSplitType.median], maxDepth: Optional(-1), pruned: false))
+
+   tree(StarCore.DecisionTreeParams(name: "937abe05", inputSequences: ["/qp/star_validated/7_6_2025-small-test-train"], positiveTrainingSize: 10282, negativeTrainingSize: 302791, decisionTypes: [StarCore.OutlierGroupFeature.size, StarCore.OutlierGroupFeature.width, StarCore.OutlierGroupFeature.height, StarCore.OutlierGroupFeature.centerX, StarCore.OutlierGroupFeature.centerY, StarCore.OutlierGroupFeature.minX, StarCore.OutlierGroupFeature.minY, StarCore.OutlierGroupFeature.maxX, StarCore.OutlierGroupFeature.maxY, StarCore.OutlierGroupFeature.hypotenuse, StarCore.OutlierGroupFeature.aspectRatio, StarCore.OutlierGroupFeature.fillAmount, StarCore.OutlierGroupFeature.surfaceAreaRatio, StarCore.OutlierGroupFeature.averagebrightness, StarCore.OutlierGroupFeature.medianBrightness, StarCore.OutlierGroupFeature.maxBrightness, StarCore.OutlierGroupFeature.maxHoughTransformCount, StarCore.OutlierGroupFeature.pixelBorderAmount, StarCore.OutlierGroupFeature.averageLineVariance, StarCore.OutlierGroupFeature.medianLineVariance, StarCore.OutlierGroupFeature.lineLength, StarCore.OutlierGroupFeature.lineIntensityScore, StarCore.OutlierGroupFeature.linePixelScore, StarCore.OutlierGroupFeature.bunchCount, StarCore.OutlierGroupFeature.medianBunchSize, StarCore.OutlierGroupFeature.maxBunchSize], decisionSplitTypes: [StarCore.DecisionSplitType.median], maxDepth: Optional(-1), pruned: false))
+
+
+ */
+
+import Foundation
+import StarCore
+
+// DO NOT EDIT THIS FILE
+// DO NOT EDIT THIS FILE
+// DO NOT EDIT THIS FILE
+
+public struct OutlierGroupForestClassifier_f9f52500: NamedOutlierGroupClassifier {
+
+    public init() { }
+
+    public let name = "f9f52500"
+    
+    public let type: ClassifierType = .forest(DecisionForestParams(name: "f9f52500",
+                                                                   treeCount: 8,
+                                                                   treeNames: [ "1c6ddc95", "4ced1c88", "427d0bd4", "73321d6c", "6fa1991d", "366885cb", "44adcedc", "937abe05"]))
+
+    let tree_1c6ddc95 = OutlierGroupDecisionTreeForest_1c6ddc95()
+    let tree_4ced1c88 = OutlierGroupDecisionTreeForest_4ced1c88()
+    let tree_427d0bd4 = OutlierGroupDecisionTreeForest_427d0bd4()
+    let tree_73321d6c = OutlierGroupDecisionTreeForest_73321d6c()
+    let tree_6fa1991d = OutlierGroupDecisionTreeForest_6fa1991d()
+    let tree_366885cb = OutlierGroupDecisionTreeForest_366885cb()
+    let tree_44adcedc = OutlierGroupDecisionTreeForest_44adcedc()
+    let tree_937abe05 = OutlierGroupDecisionTreeForest_937abe05()
+
+
+    // returns -1 for negative, +1 for positive
+    public func classification(of group: ClassifiableOutlierGroup) async -> Double {
+        var total: Double = 0.0
+
+        total += await self.tree_1c6ddc95.classification(of: group) * 0.951629942650166
+        total += await self.tree_4ced1c88.classification(of: group) * 0.9424237850890431
+        total += await self.tree_427d0bd4.classification(of: group) * 0.9615152429821914
+        total += await self.tree_73321d6c.classification(of: group) * 0.9555035717879062
+        total += await self.tree_6fa1991d.classification(of: group) * 0.9602072643123051
+        total += await self.tree_366885cb.classification(of: group) * 0.9379716269242379
+        total += await self.tree_44adcedc.classification(of: group) * 0.955704799275581
+        total += await self.tree_937abe05.classification(of: group) * 0.9270047288459604
+
+        return total / 8
+    }
+
+    // returns -1 for negative, +1 for positive
+    public func classification (
+       of features: [OutlierGroupFeature],   // parallel
+       and values: [Double]                   // arrays
+    ) async -> Double
+    {
+        var total: Double = 0.0
+        
+        let featureData = OutlierGroupFeatureData(features: features, values: values)
+        
+        total += await self.tree_1c6ddc95.classification(of: featureData) * 0.951629942650166
+        total += await self.tree_4ced1c88.classification(of: featureData) * 0.9424237850890431
+        total += await self.tree_427d0bd4.classification(of: featureData) * 0.9615152429821914
+        total += await self.tree_73321d6c.classification(of: featureData) * 0.9555035717879062
+        total += await self.tree_6fa1991d.classification(of: featureData) * 0.9602072643123051
+        total += await self.tree_366885cb.classification(of: featureData) * 0.9379716269242379
+        total += await self.tree_44adcedc.classification(of: featureData) * 0.955704799275581
+        total += await self.tree_937abe05.classification(of: featureData) * 0.9270047288459604
+
+        return total / 8
+    }
+}
