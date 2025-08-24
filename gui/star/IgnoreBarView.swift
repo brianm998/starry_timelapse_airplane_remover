901aca0641cc53ce26f71d59ca17e4ecbabbabd1
Brian Martin <brian.beholden@gmail.com>
2025-08-24 08:41:21 -0700

    add earth crop bar to IgnoreBarView

--- gui/star/IgnoreBarView.swift
+++ gui/star/IgnoreBarView.swift
@@ -40,6 +40,7 @@ public struct IgnoreBarView: View {
               .cursor(.disappearingItem)
               .onTapGesture { viewModel.showIgnoreLowerBar = false }
 
+            
             // arrow on the right
             Image(systemName: "chevron.left")
               .resizable()
@@ -87,14 +88,63 @@ public struct IgnoreBarView: View {
                   .cursor(.disappearingItem)
                   .onTapGesture { viewModel.showIgnoreLowerBar = false }
             }
+
+            // earth crop bar 
+
+            
+            // earthAlignedImageCropAmount
+            // viewModel.frameHeight/2 - viewModel.ignoreLowerPixels
+
+            
+            // arrow on the right
+            Image(systemName: "chevron.left")
+              .resizable()
+              .foregroundColor(.blue)
+              .frame(width: viewModel.arrowLength, height: viewModel.arrowHeight) 
+              .offset(x: (viewModel.frameWidth+viewModel.arrowLength)/2,
+                      y: CGFloat(viewModel.earthAlignedImageCropAmount) - (viewModel.frameHeight/2))
+              .highPriorityGesture(self.earthCropAmountGesture(adjustment: viewModel.arrowHeight/2))
+              .cursor(.resizeUpDown)
+
+            // arrow on the left
+            Image(systemName: "chevron.right")
+              .resizable()
+              .foregroundColor(.blue)
+              .frame(width: viewModel.arrowLength, height: viewModel.arrowHeight) 
+              .offset(x: -(viewModel.frameWidth+viewModel.arrowLength)/2,
+                      y: CGFloat(viewModel.earthAlignedImageCropAmount) - (viewModel.frameHeight/2))
+              .highPriorityGesture(self.earthCropAmountGesture(adjustment: viewModel.arrowHeight/2))
+              .cursor(.resizeUpDown)
+
+            // orange line at the top for gestures
+            Rectangle()
+              .opacity(0.4)
+              .background(.blue)
+              .frame(width: viewModel.frameWidth,
+                     height: viewModel.lineWidth*4)
+
+              .offset(y: CGFloat(viewModel.earthAlignedImageCropAmount) - (viewModel.frameHeight/2))
+
+            // larger clear selectable area for gesture
+            Rectangle()
+              .background(.clear)
+              .opacity(0.00001)
+              .frame(width: viewModel.frameWidth,
+                     height: viewModel.lineWidth*20)
+              .offset(y: CGFloat(viewModel.earthAlignedImageCropAmount) - (viewModel.frameHeight/2))
+//              .offset(y: viewModel.frameHeight - CGFloat(viewModel.earthAlignedImageCropAmount))//viewModel.frameHeight/2 + viewModel.lineWidth/2 - viewModel.ignoreLowerPixels)
+              .highPriorityGesture(self.earthCropAmountGesture(adjustment: viewModel.lineWidth*4))
+              .cursor(.resizeUpDown)
+
+            
         }        
     }
     
     func ignoreLowerPixelsGesture(adjustment: CGFloat) -> some Gesture {
         DragGesture() 
           .onChanged { gesture in
-              Log.d("gesture.location.y \(gesture.location.y) viewModel.frameHeight \(viewModel.frameHeight) viewModel.ignoreLowerPixels \(viewModel.ignoreLowerPixels)")
               let offset = viewModel.frameHeight/2 - gesture.location.y
+              Log.d("gesture.location.y \(gesture.location.y) viewModel.frameHeight \(viewModel.frameHeight) viewModel.ignoreLowerPixels \(viewModel.ignoreLowerPixels) offset \(offset)")
               if viewModel.ignoreLowerPixels + offset > 0 {
                   viewModel.ignoreLowerPixels = offset + adjustment
               } 
@@ -117,6 +167,38 @@ public struct IgnoreBarView: View {
                   // set the final value in the config
                   var config = configManager.config()
                   config.ignoreLowerPixels = Int(viewModel.ignoreLowerPixels)
+                  configManager.update(config)
+              }
+          }
+    }
+
+    func earthCropAmountGesture(adjustment: CGFloat) -> some Gesture {
+        DragGesture() 
+          .onChanged { gesture in
+              let offset = viewModel.frameHeight/2 - gesture.location.y
+              Log.d("gesture.location.y \(gesture.location.y) viewModel.frameHeight \(viewModel.frameHeight) viewModel.earthAlignedImageCropAmount \(viewModel.earthAlignedImageCropAmount) offset \(offset)")
+              if viewModel.earthAlignedImageCropAmount + Int(offset) > 0 {
+                  viewModel.earthAlignedImageCropAmount = Int(viewModel.frameHeight - offset + adjustment)
+              } 
+              if viewModel.earthAlignedImageCropAmount > Int(viewModel.frameHeight) {
+                  viewModel.earthAlignedImageCropAmount = Int(viewModel.frameHeight)
+              }
+          }
+          .onEnded { gesture in
+              Log.d("gesture.location.y \(gesture.location.y)")
+              let offset = viewModel.frameHeight/2 - gesture.location.y
+              if viewModel.earthAlignedImageCropAmount + Int(offset) > 0 {
+                  viewModel.earthAlignedImageCropAmount = Int(viewModel.frameHeight - offset + adjustment)
+              } else {
+                  viewModel.earthAlignedImageCropAmount = 0
+              }
+              if viewModel.earthAlignedImageCropAmount > Int(viewModel.frameHeight) {
+                  viewModel.earthAlignedImageCropAmount = Int(viewModel.frameHeight)
+              }
+              if let configManager = viewModel.config {
+                  // set the final value in the config
+                  var config = configManager.config()
+                  config.earthAlignedImageCropAmount = Int(viewModel.earthAlignedImageCropAmount)
                   configManager.update(config)
               }
           }
