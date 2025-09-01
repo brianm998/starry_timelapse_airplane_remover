e9f671d9e459428434b4eff623e4e8891e4115a6
Brian Martin <brian.beholden@gmail.com>
2025-08-31 20:13:40 -0700

    make mkdir be like shell mkdir -p

--- StarCore/Sources/StarCore/ImageSequenceProcessor.swift
+++ StarCore/Sources/StarCore/ImageSequenceProcessor.swift
@@ -7,6 +7,24 @@ import Cocoa
 // XXX move this shit
 
 public func mkdir(_ path: String) {
+    // Expand ~ and resolve any relative bits
+    let expanded = (path as NSString).expandingTildeInPath
+
+    // Use FileManager to create directory with intermediate directories (like `mkdir -p`)
+    do {
+        try FileManager.default.createDirectory(atPath: expanded,
+                                                withIntermediateDirectories: true,
+                                                attributes: nil)
+    } catch {
+        // Write an error message to stderr (non-throwing function)
+        let message = "mkdir: failed to create directory '\(expanded)': \(error.localizedDescription)\n"
+        if let data = message.data(using: .utf8) {
+            FileHandle.standardError.write(data)
+        }
+    }
+}
+/*
+public func mkdir(_ path: String) {
     if !FileManager.default.fileExists(atPath: path) {
         //Log.e("create directory at path \(path)")
         // XXX this can fail even then the file already exists
@@ -14,7 +32,7 @@ public func mkdir(_ path: String) {
                                                  withIntermediateDirectories: false,
                                                  attributes: nil)
     }
-}
+}*/
 
 // removes path from filename
 public func removePath(fromString string: String) -> String {
