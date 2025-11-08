aed53e1db67265585e06d0f33dbb8ccb0184e5a8
Brian Martin <brian.beholden@gmail.com>
2025-11-08 09:07:15 -0800

    redo the InitialInstructionsView

--- gui/star/InitialInstructionsView.swift
+++ gui/star/InitialInstructionsView.swift
@@ -5,334 +5,148 @@ import logging
 struct InitialInstructionsView: View {
     @Environment(ImageSequenceViewModel.self) var viewModel: ImageSequenceViewModel
 
-    @State private var maxConcurrentHorizonCalculationsString = ""
-    @State private var maxConcurrentHorizonCalculations: Int = 20 {
-        didSet {
-            if let config = viewModel.config {
-                var newConfig = config.config()
-                newConfig.maxConcurrentHorizonCalculations = maxConcurrentHorizonCalculations
-                config.update(newConfig)
-            }
-        }
-    }
-
-    @State private var pixelReplacementMethod: PixelReplacementMethod = .automatic {
-        didSet {
-            if let config = viewModel.config {
-                var newConfig = config.config()
-                newConfig.pixelReplacementMethod = pixelReplacementMethod
-                config.update(newConfig)
-            }
-        }
-    }
+    @State private var horizonDetectionEnabled = true
+    @State private var tripodHeadWasMoving = false
+    @State private var automaticProcessing = true
     
     var body: some View {
         @Bindable var viewModel = viewModel
         return 
           VStack {
-              Text("You've just loaded a new image sequence")
+              Text("How should Star process this video?")
                 .font(.largeTitle)
                 .foregroundColor(.white)
 
-              Spacer()
-                .frame(maxHeight: 10)
+              Grid {
 
-
-              ScrollView {
-                  Text(
-"""
-Star offers horizon detection which increases the output quality of videos that include both sky and the earth.
-
-It is best to keep it on unless your video is sky only.  If your video is only sky, then horizon detection is not necessary and will just slow things down.
-"""
-                  )
-              }
-                .font(.body)
-                .foregroundColor(.white)
-                .fixedSize(horizontal: false, vertical: true)
-
-
-              Text("Is this video of the sky only?")
-                .font(.largeTitle)
-                .foregroundColor(.white)
-              
-
-              HStack {
-                  Button() {
-                      viewModel.horizonDetectionEnabled = true
-                      if let config = viewModel.config {
-                          // force the config to save itself now
-                          // otherwise the horizonDetectionEnabled may
-                          // still be nil
-                          var newConfig = config.config()
-                          newConfig.horizonDetectionEnabled = viewModel.horizonDetectionEnabled
-                          config.update(newConfig)
-                      }
-                      
-                  } label: {
-                      ZStack {
-                          if viewModel.horizonDetectionEnabled {
-                              Color.white
-                                .cornerRadius(20)
-                              
-                              Text("Yes")
-                                .font(.title2)
-                                .padding(20)
-                          } else {
-                              Color.blue
-                                .cornerRadius(20)
-                              
-                              Text("No")
-                                .foregroundColor(.white)
-                                .font(.title2)
-                                .padding(20)
-                          }
-                      }
-                        .fixedSize(horizontal: true, vertical: true)
-                  }
-                    .buttonStyle(PlainButtonStyle()) // XXX these styles suck
-                  
-                  Button() {
-                      viewModel.horizonDetectionEnabled = false
-                      if let config = viewModel.config {
-                          // force the config to save itself now
-                          // otherwise the horizonDetectionEnabled may
-                          // still be nil
-                          var newConfig = config.config()
-                          newConfig.horizonDetectionEnabled = viewModel.horizonDetectionEnabled
-                          config.update(newConfig)
-                      }
-                      
-                  } label: {
-                      ZStack {
-                          if viewModel.horizonDetectionEnabled {
-                              Color.blue
-                                .cornerRadius(20)
-                              
-                              Text("No")
-                                .foregroundColor(.white)
-                                .font(.title2)
-                                .padding(20)
-                          } else {
-                              Color.white
-                                .cornerRadius(20)
-                              
-                              Text("No")
-                                .font(.title2)
-                                .padding(20)
-                          }
-                      }
-                        .fixedSize(horizontal: true, vertical: true)
-                  }
-                    .buttonStyle(PlainButtonStyle()) // XXX these styles suck
-              }
-
-              ScrollView {
-                  Text(
-"""
-Star has offers two similar, but different ways to process this sequence of images:
-1. Automatic
-2. Selective
-
-Both use the same initial set of processing steps:
-
- - horizon detection
- - neighboring images star and earth aligned for each frame
- - aligned images combined with custom median logic at each pixel
-
- Then, Automatic simply uses the combined aligned images as the direct output.  This will likely make small changes to all pixels in every frame.
-
-Automatic mode is good because:
- - no user interaction is required for each frame
- - it treats gets literally all airplane and satellite signals as noise and removes it
- - it also reduces the regular noise floor of each frame, making them cleaner
- - less computer processing is required than selective mode
- 
-Automatic is not right if:
- - There are much clouds in the sky
- - you want to keep some signals in the video like meteors, satellites, etc.
-
-Selective mode does more processing after computing the combined aligned image for each frame.
-This processing eventually results in a layer mask which is used by Star to apply just certain parts of the combined aligned image to the frame being processed.
-
-Selective mode is good because:
- - the user is able to specify exactly what changes to make to each frame
- - more of the original pixels are kept as is
- - clouds in the sky can be ok
-
-Selective mode is not right if:
- - your video has no clouds in the sky and you want all human produced noise removed (use Automatic mode)
- - you don't want to have to verify the edits on each frame
-
-"""
-                  )
-              }
-                .font(.body)
-                .foregroundColor(.white)
-                .fixedSize(horizontal: false, vertical: true)
-              
-              Space(height: 20)
-              
-              Text("What processing method do you want to use for this image sequence?")
-                .font(.largeTitle)
-                .foregroundColor(.white)
-              
-              HStack {
-                  
-                  Button() {
-                      self.pixelReplacementMethod = .automatic
-                  } label: {
-                      ZStack {
-                          switch self.pixelReplacementMethod {
-                          case .automatic:
-                              Color.blue
-                                .cornerRadius(20)
-
-                              Text("Automatic")
-                                .foregroundColor(.white)
-                                .font(.title2)
-                                .padding(20)
-                              
-                          case .selective:
-                              Color.white
-                                .cornerRadius(20)
-                              Text("Automatic")
-
-                                .font(.title2)
-                                .padding(20)
-                          }
-                      }
-                        .fixedSize(horizontal: true, vertical: true)
-                  }
-                    .buttonStyle(PlainButtonStyle()) // XXX these styles suck
-                  
-                  Button() {
-                      self.pixelReplacementMethod = .selective
-                  } label: {
-                      ZStack {
-                          switch self.pixelReplacementMethod {
-                          case .selective:
-                              Color.blue
-                                .cornerRadius(20)
-
-                              Text("Selective")
-                                .foregroundColor(.white)
-                                .font(.title2)
-                                .padding(20)
-                              
-                          case .automatic:
-                              Color.white
-                                .cornerRadius(20)
-                              Text("Selective")
-
-                                .font(.title2)
-                                .padding(20)
-                          }
-                      }
-                        .fixedSize(horizontal: true, vertical: true)
-                  }
-                    .buttonStyle(PlainButtonStyle()) // XXX these styles suck
-              }
-              //                }
-              //                  .padding(20)
-              //                  .background(.gray)
-              //                  .cornerRadius(20)
-
-              Space(height: 20)
-
-              Button() {
-                  viewModel.shouldShowInitialInstructions = false
-                  viewModel.showIgnoreLowerBar = false
-
-                  if viewModel.horizonDetectionEnabled {
-
-                      viewModel.processHorizonForAllFrames() {
-                          Log.d("FUCKING CLOSURE CALLED")
-                          // after we get horizons for all frames, then either
-                          switch self.pixelReplacementMethod {
-                          case .automatic:
-                              // render all frames automatically
-                              viewModel.renderAllFramesAutomatic()
-                              
-                          case .selective:
-                              // or show a dialog to tell new users what to do next
-                              viewModel.showProcessingOptionsSheet = true
-                          }
-                      }
-                  } else {
-
-                      viewModel.ignoreLowerPixels = 0
-
-                      switch self.pixelReplacementMethod {
-                      case .automatic:
-                          // render all frames automatically
-                          viewModel.renderAllFramesAutomatic()
-                          
-                      case .selective:
-                          // or show a dialog to tell new users what to do next
-                          viewModel.showProcessingOptionsSheet = true
-                      }
-                  }
-              } label: {
-                  ZStack {
-                      Color.blue
-                        .cornerRadius(20)
-
+                  GridRow {
                       HStack {
+                          Spacer()
+                          Text("With horizon detection")
+                            .font(.title2)
+                            .foregroundColor(.white)
+                      }
+                      Toggle("", isOn: $horizonDetectionEnabled)
+                  }
+                  GridRow {
+                      HStack {
+                          Spacer()
+                          Text("Using moving tripod head")
+                            .font(.title2)
+                            .foregroundColor(horizonDetectionEnabled ? .white : .black)
+                      }
+                      Toggle("", isOn: $tripodHeadWasMoving)
+                  }
+                    .disabled(!horizonDetectionEnabled)
+                  GridRow {
+                      HStack {
+                          Spacer()
+                          Text("Automatically")
+                            .font(.title2)
+                            .foregroundColor(.white)
+                      }
+                      Toggle("", isOn: $automaticProcessing)
+                  }
+              }
+                .fixedSize(horizontal: true, vertical: false)
+
+              Space(height: 20)
+              
+              Text("Automatic processing removes all airplanes and satellites, as well as moving car headlights on the ground.  Works best with clear skies.")
+                .font(.title3)
+                .foregroundColor(.white)
+              
+              Text("Not using Automatic processing removes only the signals you want removed, allowing more control.  Supports moving clouds better.  Requires more user interaction before final rendering.")
+                .font(.title3)
+                .foregroundColor(.white)
+
+              Space(height: 20)
+
+              HStack {
+                  Button {
+                      viewModel.shouldShowInitialInstructions = false
+                  } label: {
+                      ZStack {
+                          Color.white
+                            .cornerRadius(20)
+
+                          Text("Cancel")
+                            .font(.title2)
+                            .padding(20)
+                      }
+                  }
+                    .buttonStyle(PlainButtonStyle()) // XXX these styles suck
+
+                  Button {
+                      startProcessing()
+                      
+                  } label: {
+                      ZStack {
+                          Color.blue
+                            .cornerRadius(20)
 
                           Text("Start Processing")
-                            .foregroundColor(.white)
                             .font(.title2)
-
-                          switch self.pixelReplacementMethod {
-                          case .automatic:
-                              // render all frames automatically
-                              Text("automatically")
-                                .font(.title2)
-                                .foregroundColor(.white)
-                              
-                          case .selective:
-                              // or show a dialog to tell new users what to do next
-                              Text("with both user and machine learning selection")
-                                .font(.title2)
-                                .foregroundColor(.white)
-                          }                                                    
-                          
-                          if viewModel.horizonDetectionEnabled {
-                              Text("using horizon detection")
-                                .font(.title2)
-                                .foregroundColor(.white)
-                          }
+                            .padding(20)
+                            .foregroundColor(.white)
                       }
-                        .fixedSize(horizontal: true, vertical: true)
-                        .padding(20)
                   }
-                    .fixedSize(horizontal: true, vertical: true)
+                    .buttonStyle(PlainButtonStyle()) // XXX these styles suck
               }
-                .buttonStyle(PlainButtonStyle()) // XXX these styles suck
+                .fixedSize(horizontal: true, vertical: true)
           }
           .padding(20)
           .background(.gray)
           .cornerRadius(20)
-          .fixedSize(horizontal: false, vertical: false) // let it grow vertically, not horizonta
+    }
+    
+
+    private func startProcessing() {
+        Log.d("Start")
+
+        let pixelReplacementMethod: PixelReplacementMethod = automaticProcessing ? .automatic : .selective
         
-        //            Spacer()
-        //              .frame(maxWidth: 200)
+        if let config = viewModel.config {
+            var newConfig = config.config()
+            newConfig.pixelReplacementMethod = pixelReplacementMethod
+            newConfig.horizonDetectionEnabled = horizonDetectionEnabled
+            newConfig.tripodHeadWasMoving = tripodHeadWasMoving
+            config.update(newConfig)
+        }
+        viewModel.horizonDetectionEnabled = horizonDetectionEnabled
+        viewModel.shouldShowInitialInstructions = false
 
 
-        
-        //        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        //        Spacer()
-        //          .frame(maxHeight: 200)
+        viewModel.showIgnoreLowerBar = false
 
+        if viewModel.horizonDetectionEnabled {
 
-        //.frame(maxWidth: .infinity, maxHeight: .infinity)
-          .onAppear {
-              if let config = viewModel.config {
-                  var newConfig = config.config()
-                  newConfig.maxConcurrentHorizonCalculations = maxConcurrentHorizonCalculations
-                  config.update(newConfig)
-              }
-          }
+            viewModel.processHorizonForAllFrames() {
+                Log.d("FUCKING CLOSURE CALLED")
+                // after we get horizons for all frames, then either
+                switch pixelReplacementMethod {
+                case .automatic:
+                    // render all frames automatically
+                    viewModel.renderAllFramesAutomatic()
+                    
+                case .selective:
+                    // or show a dialog to tell new users what to do next
+                    viewModel.showProcessingOptionsSheet = true
+                }
+            }
+        } else {
+
+            viewModel.ignoreLowerPixels = 0
+
+            switch pixelReplacementMethod {
+            case .automatic:
+                // render all frames automatically
+                viewModel.renderAllFramesAutomatic()
+                
+            case .selective:
+                // or show a dialog to tell new users what to do next
+                viewModel.showProcessingOptionsSheet = true
+            }
+        }
     }
 }
