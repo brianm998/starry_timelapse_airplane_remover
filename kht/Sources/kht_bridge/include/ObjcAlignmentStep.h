864cf5478884de164ed23f694891751587deb2a0
Brian Martin <brian.beholden@gmail.com>
2026-01-25 10:04:02 -0800

    remove unused alignment step

--- kht/Sources/kht_bridge/include/ObjcAlignmentStep.h
+++ kht/Sources/kht_bridge/include/ObjcAlignmentStep.h
@@ -4,11 +4,10 @@ typedef NS_ENUM(NSInteger, ObjCAlignmentStep) {
   ObjCAlignmentStepStart = 0,
   ObjCAlignmentStepBaseKeypointDetection = 1,
   ObjCAlignmentStepBaseKeypointDetectionComplete = 2,
-  ObjCAlignmentStepLoadingNeighbors = 3,
-  ObjCAlignmentStepNeighborKeypointDetection = 4,
-  ObjCAlignmentStepNeighborKeypointMatch = 5,
-  ObjCAlignmentStepAligningNeighbor = 6,
-  ObjCAlignmentStepLoadingNeighbor = 7,
-  ObjCAlignmentStepComplete = 8,
+  ObjCAlignmentStepNeighborKeypointDetection = 3,
+  ObjCAlignmentStepNeighborKeypointMatch = 4,
+  ObjCAlignmentStepAligningNeighbor = 5,
+  ObjCAlignmentStepLoadingNeighbor = 6,
+  ObjCAlignmentStepComplete = 7,
 };
 
