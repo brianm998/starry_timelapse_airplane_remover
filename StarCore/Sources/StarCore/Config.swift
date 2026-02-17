57d03953d6f5234234c157fcbee1a05af33ab861
Brian Martin <brian.beholden@gmail.com>
2026-02-16 21:27:13 -0800

    tweak horizonSearchShrinkFactor

--- StarCore/Sources/StarCore/Config.swift
+++ StarCore/Sources/StarCore/Config.swift
@@ -323,7 +323,7 @@ public struct Config: Codable, Sendable, Transferable {
     // How much to shrink the image for the initial parameter search pass.
     // 4 means 1/4 resolution, 8 means 1/8 resolution, etc.
     // Higher values are faster but less precise for parameter selection.
-    public var horizonSearchShrinkFactor: Int = 8
+    public var horizonSearchShrinkFactor: Int = 6
 
     // [min, max] bounds for the crop percentage search range.
     // Each value is a percentage (0-100) of the image height to ignore from the top.
