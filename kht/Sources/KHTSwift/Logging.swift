6ed4ade306a169dea1953675812c3aec233cd3ad
Brian Martin <brian.beholden@gmail.com>
2026-03-29 15:23:09 -0700

    pass through line numbers and file names when logging from c++

--- kht/Sources/KHTSwift/Logging.swift
+++ kht/Sources/KHTSwift/Logging.swift
@@ -7,16 +7,19 @@ import logging
 public func setupKHTLogging() {
     kht_bridge_set_log_handler { message, level, file, function, line in
         guard let message else { return }
-        let msg = String(cString: message)
-        let lvl = level.flatMap { String(cString: $0) } ?? "info"
+        let msg  = String(cString: message)
+        let lvl  = level.flatMap    { String(cString: $0) } ?? "info"
+        let fStr = file.flatMap     { String(cString: $0) } ?? "<unknown>"
+        let fnStr = function.flatMap { String(cString: $0) } ?? ""
+        let lineInt = Int(line)
 
         switch lvl {
-        case "verbose": Log.v(msg)
-        case "debug":   Log.d(msg)
-        case "info":    Log.i(msg)
-        case "warn":    Log.w(msg)
-        case "error":   Log.e(msg)
-        default:        Log.i(msg)
+        case "verbose": Log.v(msg, file: fStr, function: fnStr, line: lineInt)
+        case "debug":   Log.d(msg, file: fStr, function: fnStr, line: lineInt)
+        case "info":    Log.i(msg, file: fStr, function: fnStr, line: lineInt)
+        case "warn":    Log.w(msg, file: fStr, function: fnStr, line: lineInt)
+        case "error":   Log.e(msg, file: fStr, function: fnStr, line: lineInt)
+        default:        Log.i(msg, file: fStr, function: fnStr, line: lineInt)
         }
     }
 }
